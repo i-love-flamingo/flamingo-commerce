aa0868497ee3e1c3b348033f41c7fb1218e452cf
Bastian Ike <bastian.ike@aoe.com>
2017-02-22 13:06:01 +0100

    Flamingo: refactor structure, clean up, move logical related objects in subpackages, extract templatefuntions into core/template

--- product/register.go
+++ product/register.go
@@ -1,11 +1,11 @@
 package product
 
 import (
-	"flamingo/core/flamingo"
+	"flamingo/core/flamingo/service_container"
 	"flamingo/core/product/controller"
 )
 
-func Register(r *flamingo.ServiceContainer) {
+func Register(r *service_container.ServiceContainer) {
 	r.Handle("product.view", new(controller.ViewController))
 	r.Route("/product/{Uid}", "product.view")
 }
