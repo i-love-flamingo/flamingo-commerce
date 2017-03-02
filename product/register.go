cfeb37cd1dbaf1a20ab8eadd2247f9380cd2edd6
Bastian Ike <bastian.ike@aoe.com>
2017-03-02 16:25:46 +0100

    127534: refactory flaming/service_container to flamingo/dependencyinjection

--- product/register.go
+++ product/register.go
@@ -1,12 +1,15 @@
 package product
 
 import (
-	"flamingo/core/flamingo/service_container"
+	di "flamingo/core/flamingo/dependencyinjection"
+	"flamingo/core/flamingo/router"
 	"flamingo/core/product/controller"
 )
 
 // Register Services for product package
-func Register(r *service_container.ServiceContainer) {
-	r.Handle("product.view", new(controller.ViewController))
-	r.Route("/product/{Uid}", "product.view")
+func Register(c *di.Container) {
+	c.Register(func(r *router.Router) {
+		r.Handle("product.view", new(controller.ViewController))
+		r.Route("/product/{Uid}", "product.view")
+	}, router.RouterRegister)
 }
