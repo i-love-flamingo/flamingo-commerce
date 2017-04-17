624443200401c6bddf19f55cae56d7dda9c9a668
Bastian Ike <bastian.ike@aoe.com>
2017-04-17 22:57:40 +0200

    131637: rewrite flamingo di, decouple, use proper DI

---
+++ product/module.go
@@ -0,0 +1,19 @@
+package product
+
+import (
+	"flamingo/core/dingo"
+	"flamingo/core/product/controller"
+	"flamingo/framework/router"
+)
+
+type (
+	// Module registers our profiler
+	Module struct {
+		RouterRegistry *router.RouterRegistry `inject:""`
+	}
+)
+
+func (m *Module) Configure(injector *dingo.Injector) {
+	m.RouterRegistry.Handle("product.view", new(controller.ViewController))
+	m.RouterRegistry.Route("/product/{uid}", "product.view")
+}
