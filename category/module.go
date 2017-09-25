8a26f40f6f4da4121c28f9386749d29b0e408ede
Bastian Ike <bastian.ike@aoe.com>
2017-09-25 16:07:02 +0200

    product, category: simplify URLs

--- category/module.go
+++ category/module.go
@@ -14,6 +14,16 @@ type (
 	}
 )
 
+// URL to category
+func URL(code string) (string, map[string]string) {
+	return "category.view", map[string]string{"code": code}
+}
+
+// URL with name to category
+func URLWithName(code, name string) (string, map[string]string) {
+	return "category.view", map[string]string{"code": code, "name": name}
+}
+
 // Configure the product URL
 func (m *Module) Configure(injector *dingo.Injector) {
 	m.RouterRegistry.Handle("category.view", new(controller.View))
