cd8b09c3e483a5edbae4e72c52ce4be2e70aa435
Daniel Pötzinger <daniel.poetzinger@aoe.com>
2018-01-04 12:31:12 +0100

    prepare controller for user checkout

--- checkout/interfaces/controller/checkoutcontroller.go
+++ checkout/interfaces/controller/checkoutcontroller.go
@@ -23,7 +23,7 @@ type (
 		Form                 domain.Form
 		CartValidationResult cart.CartValidationResult
 		ErrorMessage         string
-		HasFormError         bool
+		HasSubmitError       bool
 	}
 
 	// SuccessViewData represents the success view data
@@ -36,14 +36,14 @@ type (
 	CheckoutController struct {
 		responder.RenderAware   `inject:""`
 		responder.RedirectAware `inject:""`
-		ApplicationCartService  application.CartService     `inject:""`
-		PaymentService          application2.PaymentService `inject:""`
-		UserService             application3.UserService    `inject:""`
-		Router                  *router.Router              `inject:""`
-		CheckoutFormService     domain.FormService          `inject:""`
-		Logger                  flamingo.Logger             `inject:""`
-		SourcingEngine          application2.SourcingEngine `inject:""`
-		OrderService            application2.OrderService   `inject:""`
+		ApplicationCartService  application.CartService      `inject:""`
+		PaymentService          application2.PaymentService  `inject:""`
+		UserService             application3.UserService     `inject:""`
+		Router                  *router.Router               `inject:""`
+		CheckoutFormService     *formDto.CheckoutFormService `inject:""`
+		Logger                  flamingo.Logger              `inject:""`
+		SourcingEngine          application2.SourcingEngine  `inject:""`
+		OrderService            application2.OrderService    `inject:""`
 	}
 )
 
@@ -74,78 +74,18 @@ func (cc *CheckoutController) StartAction(ctx web.Context) web.Response {
 
 	return cc.Render(ctx, "checkout/startcheckout", CheckoutViewData{
 		DecoratedCart: decoratedCart,
-		HasFormError:  false,
 	})
 }
 
 // SubmitUserCheckoutAction handles the user order submit
 // TODO: implement this
 func (cc *CheckoutController) SubmitUserCheckoutAction(ctx web.Context) web.Response {
-	return cc.SubmitGuestCheckoutAction(ctx)
+	return cc.submitOrderForm(ctx, cc.CheckoutFormService, "checkout/usercheckout")
 }
 
 // SubmitGuestCheckoutAction handles the guest order submit
 func (cc *CheckoutController) SubmitGuestCheckoutAction(ctx web.Context) web.Response {
-
-	//Guard Clause if Cart cannout be fetched
-	decoratedCart, e := cc.ApplicationCartService.GetDecoratedCart(ctx)
-	if e != nil {
-		cc.Logger.Errorf("cart.checkoutcontroller.viewaction: Error %v", e)
-		return cc.Render(ctx, "checkout/carterror", nil)
-	}
-
-	if cc.CheckoutFormService == nil {
-		cc.Logger.Error("cart.checkoutcontroller.viewaction: Error CheckoutFormService not present!", e)
-		return cc.Render(ctx, "checkout/carterror", nil)
-	}
-
-	form, e := formApplicationService.ProcessFormRequest(ctx, cc.CheckoutFormService)
-	// return on error (template need to handle error display)
-	if e != nil {
-		return cc.Render(ctx, "checkout/checkout", CheckoutViewData{
-			DecoratedCart:        decoratedCart,
-			CartValidationResult: cc.ApplicationCartService.ValidateCart(ctx, decoratedCart),
-			Form:                 form,
-		})
-	}
-
-	//Guard Clause if Cart is empty
-	if decoratedCart.Cart.ItemCount() == 0 {
-		return cc.Render(ctx, "checkout/checkout", CheckoutViewData{
-			DecoratedCart:        decoratedCart,
-			CartValidationResult: cc.ApplicationCartService.ValidateCart(ctx, decoratedCart),
-			Form:                 form,
-		})
-	}
-
-	if form.IsValidAndSubmitted() {
-		if checkoutFormData, ok := form.Data.(formDto.CheckoutFormData); ok {
-			orderID, err := cc.placeOrder(ctx, checkoutFormData, decoratedCart)
-			if err != nil {
-				return cc.Render(ctx, "checkout/checkout", CheckoutViewData{
-					DecoratedCart:        decoratedCart,
-					CartValidationResult: cc.ApplicationCartService.ValidateCart(ctx, decoratedCart),
-					HasFormError:         true,
-					Form:                 form,
-					ErrorMessage:         err.Error(),
-				})
-			}
-			shippingEmail := checkoutFormData.ShippingAddress.Email
-			if shippingEmail == "" {
-				shippingEmail = checkoutFormData.BillingAddress.Email
-			}
-			return cc.Redirect("checkout.success", nil).With("checkout.success.data", SuccessViewData{
-				OrderId: orderID,
-				Email:   shippingEmail,
-			})
-		}
-	}
-
-	return cc.Render(ctx, "checkout/checkout", CheckoutViewData{
-		DecoratedCart:        decoratedCart,
-		CartValidationResult: cc.ApplicationCartService.ValidateCart(ctx, decoratedCart),
-		Form:                 form,
-	})
+	return cc.submitOrderForm(ctx, cc.CheckoutFormService, "checkout/guestcheckout")
 }
 
 // SuccessAction handles the order success action
@@ -158,9 +98,74 @@ func (cc *CheckoutController) SuccessAction(ctx web.Context) web.Response {
 	return cc.Render(ctx, "checkout/expired", nil)
 }
 
-func (cc *CheckoutController) placeOrder(ctx web.Context, checkoutFormData formDto.CheckoutFormData, decoratedCart cart.DecoratedCart) (string, error) {
+func (cc *CheckoutController) submitOrderForm(ctx web.Context, formservice *formDto.CheckoutFormService, template string) web.Response {
 
+	//Guard Clause if Cart cannout be fetched
+	decoratedCart, e := cc.ApplicationCartService.GetDecoratedCart(ctx)
+	if e != nil {
+		cc.Logger.Errorf("cart.checkoutcontroller.submitaction: Error %v", e)
+		return cc.Render(ctx, "checkout/carterror", nil)
+	}
+
+	if formservice == nil {
+		cc.Logger.Error("cart.checkoutcontroller.submitaction: Error CheckoutFormService not present!")
+		return cc.Render(ctx, "checkout/carterror", nil)
+	}
+
+	form, e := formApplicationService.ProcessFormRequest(ctx, formservice)
+	// return on error (template need to handle error display)
+	if e != nil {
+		return cc.Render(ctx, template, CheckoutViewData{
+			DecoratedCart:        decoratedCart,
+			CartValidationResult: cc.ApplicationCartService.ValidateCart(ctx, decoratedCart),
+			Form:                 form,
+		})
+	}
+
+	//Guard Clause if Cart is empty
+	if decoratedCart.Cart.ItemCount() == 0 {
+		return cc.Render(ctx, template, CheckoutViewData{
+			DecoratedCart:        decoratedCart,
+			CartValidationResult: cc.ApplicationCartService.ValidateCart(ctx, decoratedCart),
+			Form:                 form,
+		})
+	}
+
+	if form.IsValidAndSubmitted() {
+		if checkoutFormData, ok := form.Data.(formDto.CheckoutFormData); ok {
+			orderID, err := cc.placeOrder(ctx, checkoutFormData, decoratedCart)
+			if err != nil {
+				//Place Order Error
+				return cc.Render(ctx, template, CheckoutViewData{
+					DecoratedCart:        decoratedCart,
+					CartValidationResult: cc.ApplicationCartService.ValidateCart(ctx, decoratedCart),
+					HasSubmitError:       true,
+					Form:                 form,
+					ErrorMessage:         err.Error(),
+				})
+			}
+			shippingEmail := checkoutFormData.ShippingAddress.Email
+			if shippingEmail == "" {
+				shippingEmail = checkoutFormData.BillingAddress.Email
+			}
+			return cc.Redirect("checkout.success", nil).With("checkout.success.data", SuccessViewData{
+				OrderId: orderID,
+				Email:   shippingEmail,
+			})
+		} else {
+			cc.Logger.Error("cart.checkoutcontroller.submitaction: Error cannot type convert to CheckoutFormData!")
+			return cc.Render(ctx, "checkout/carterror", nil)
+		}
+	}
+	//Default: Form not submitted yet or submitted with validation errors:
+	return cc.Render(ctx, template, CheckoutViewData{
+		DecoratedCart:        decoratedCart,
+		CartValidationResult: cc.ApplicationCartService.ValidateCart(ctx, decoratedCart),
+		Form:                 form,
+	})
+}
+
+func (cc *CheckoutController) placeOrder(ctx web.Context, checkoutFormData formDto.CheckoutFormData, decoratedCart cart.DecoratedCart) (string, error) {
 	billingAddress, shippingAddress := formDto.MapAddresses(checkoutFormData)
-	_ = shippingAddress
-	return cc.OrderService.PlaceOrder(ctx, decoratedCart, "ispu", "ispu", billingAddress, billingAddress)
+	return cc.OrderService.PlaceOrder(ctx, decoratedCart, "ispu", "ispu", billingAddress, shippingAddress)
 }
