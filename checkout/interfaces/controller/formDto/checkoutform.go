9f63bf24dc37667470880f1176f9f27bda937c7c
Daniel Pötzinger <daniel.poetzinger@aoe.com>
2018-01-20 20:52:26 +0100

    BUG #154110 email get preset overrides

--- checkout/interfaces/controller/formDto/checkoutform.go
+++ checkout/interfaces/controller/formDto/checkoutform.go
@@ -63,7 +63,7 @@ func (fs *CheckoutFormService) ParseFormData(ctx web.Context, formValues url.Val
 	// Preset eMail when email parameter is given:
 	if ctx != nil {
 		email, e := ctx.Query("email")
-		if e == nil {
+		if e == nil && len(formValues["billingAddress.email"]) == 0 {
 			formValues["billingAddress.email"] = email
 		}
 	}
