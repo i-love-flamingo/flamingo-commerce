762f4659d85012100bb7c8f6333aa1181697d2c2
Joachim Adomeit <joachim.adomeit@aoe.com>
2018-03-07 14:06:58 +0100

    Codestyle improvements

--- cart/application/cartservice.go
+++ cart/application/cartservice.go
@@ -3,7 +3,7 @@ package application
 import (
 	"github.com/pkg/errors"
 	"go.aoe.com/flamingo/core/auth/application"
-	domaincart "go.aoe.com/flamingo/core/cart/domain/cart"
+	cartDomain "go.aoe.com/flamingo/core/cart/domain/cart"
 	productDomain "go.aoe.com/flamingo/core/product/domain"
 	"go.aoe.com/flamingo/framework/flamingo"
 	"go.aoe.com/flamingo/framework/web"
@@ -12,16 +12,16 @@ import (
 // CartService application struct
 type (
 	CartService struct {
-		GuestCartService     domaincart.GuestCartService      `inject:""`
-		CustomerCartService  domaincart.CustomerCartService   `inject:""`
-		CartDecoratorFactory *domaincart.DecoratedCartFactory `inject:""`
+		GuestCartService     cartDomain.GuestCartService      `inject:""`
+		CustomerCartService  cartDomain.CustomerCartService   `inject:""`
+		CartDecoratorFactory *cartDomain.DecoratedCartFactory `inject:""`
 		ProductService       productDomain.ProductService     `inject:""`
 		Logger               flamingo.Logger                  `inject:""`
-		CartValidator        domaincart.CartValidator         `inject:",optional"`
+		CartValidator        cartDomain.CartValidator         `inject:",optional"`
 		AuthManager          *application.AuthManager         `inject:""`
 		UserService          *application.UserService         `inject:""`
 
-		ItemValidator domaincart.ItemValidator `inject:",optional"`
+		ItemValidator cartDomain.ItemValidator `inject:",optional"`
 
 		//DefaultDeliveryMethodForValidation - used for calling the CartValidator (this is something that might get obsolete if the Cart and the CartItems have theire Deliverymethod "saved")
 		DefaultDeliveryMethodForValidation string `inject:"config:cart.validation.defaultDeliveryMethod,optional"`
@@ -29,18 +29,18 @@ type (
 )
 
 // Auth tries to retrieve the authentication context for a active session
-func (cs *CartService) Auth(c web.Context) domaincart.Auth {
+func (cs *CartService) Auth(c web.Context) cartDomain.Auth {
 	ts, _ := cs.AuthManager.TokenSource(c)
 	idToken, _ := cs.AuthManager.IDToken(c)
 
-	return domaincart.Auth{
+	return cartDomain.Auth{
 		TokenSource: ts,
 		IDToken:     idToken,
 	}
 }
 
 // GetCart Get the correct Cart
-func (cs *CartService) GetCart(ctx web.Context) (domaincart.Cart, error) {
+func (cs *CartService) GetCart(ctx web.Context) (cartDomain.Cart, error) {
 	if cs.isLoggedIn(ctx) {
 		return cs.CustomerCartService.GetCart(ctx, cs.Auth(ctx), "me")
 	}
@@ -48,6 +48,7 @@ func (cs *CartService) GetCart(ctx web.Context) (domaincart.Cart, error) {
 	guestCart, err := cs.GetSessionGuestCart(ctx)
 	if err != nil {
 		cs.Logger.Warn("cart.application.cartservice: GetCart - No cart in session return empty")
+
 		return cs.getEmptyCart()
 	}
 
@@ -55,28 +56,32 @@ func (cs *CartService) GetCart(ctx web.Context) (domaincart.Cart, error) {
 }
 
 // ValidateCart validates a carts content
-func (cs CartService) ValidateCart(ctx web.Context, decoratedCart domaincart.DecoratedCart) domaincart.CartValidationResult {
+func (cs CartService) ValidateCart(ctx web.Context, decoratedCart cartDomain.DecoratedCart) cartDomain.CartValidationResult {
 	if cs.CartValidator != nil {
 		// TODO pass delivery Method from CART - once cart supports this!
 		result := cs.CartValidator.Validate(ctx, decoratedCart, cs.DefaultDeliveryMethodForValidation)
+
 		return result
 	}
-	return domaincart.CartValidationResult{}
+
+	return cartDomain.CartValidationResult{}
 }
 
 // GetDecoratedCart Get the correct Cart
-func (cs *CartService) GetDecoratedCart(ctx web.Context) (domaincart.DecoratedCart, error) {
-	var empty domaincart.DecoratedCart
-	cart, err := cs.GetCart(ctx)
+func (cs *CartService) GetDecoratedCart(ctx web.Context) (cartDomain.DecoratedCart, error) {
+	var empty cartDomain.DecoratedCart
 	cs.Logger.Info("cart.application.cartservice: Get decorated cart ")
+
+	cart, err := cs.GetCart(ctx)
 	if err != nil {
 		return empty, err
 	}
+
 	return *cs.CartDecoratorFactory.Create(ctx, cart), nil
 }
 
 // AddProduct Add a product
-func (cs *CartService) AddProduct(ctx web.Context, addRequest domaincart.AddRequest) error {
+func (cs *CartService) AddProduct(ctx web.Context, addRequest cartDomain.AddRequest) error {
 	addRequest, err := cs.checkProductAndEnrichAddRequest(ctx, addRequest)
 	if err != nil {
 		return err
@@ -102,7 +107,7 @@ func (cs *CartService) AddProduct(ctx web.Context, addRequest domaincart.AddRequ
 }
 
 // checkProductAndEnrichAddRequest existence and validate with productService
-func (cs *CartService) checkProductAndEnrichAddRequest(ctx web.Context, addRequest domaincart.AddRequest) (domaincart.AddRequest, error) {
+func (cs *CartService) checkProductAndEnrichAddRequest(ctx web.Context, addRequest cartDomain.AddRequest) (cartDomain.AddRequest, error) {
 	product, err := cs.ProductService.Get(ctx, addRequest.MarketplaceCode)
 	if err != nil {
 		return addRequest, errors.New("cart.application.cartservice - AddProduct:Product not found")
@@ -129,7 +134,7 @@ func (cs *CartService) checkProductAndEnrichAddRequest(ctx web.Context, addReque
 }
 
 // addProductToGuestCart Handle Adding to Guest Cart
-func (cs *CartService) addProductToGuestCart(ctx web.Context, addRequest domaincart.AddRequest) error {
+func (cs *CartService) addProductToGuestCart(ctx web.Context, addRequest cartDomain.AddRequest) error {
 	//check if we have a guest cart in the session
 	guestCart, err := cs.GetSessionGuestCart(ctx)
 
@@ -159,6 +164,7 @@ func (cs *CartService) addProductToGuestCart(ctx web.Context, addRequest domainc
 	cs.publishAddtoCartEvent(ctx, guestCart, addRequest)
 
 	cs.Logger.Infof("cart.application.cartservice: Added to cart %s", guestCartID)
+
 	return nil
 }
 
@@ -176,15 +182,17 @@ func (cs *CartService) HasSessionAGuestCart(ctx web.Context) bool {
 }
 
 // GetSessionGuestCart
-func (cs *CartService) GetSessionGuestCart(ctx web.Context) (domaincart.Cart, error) {
-	var cart domaincart.Cart
+func (cs *CartService) GetSessionGuestCart(ctx web.Context) (cartDomain.Cart, error) {
+	var cart cartDomain.Cart
 	if guestcartid, ok := ctx.Session().Values["cart.guestid"]; ok {
 		existingCart, err := cs.GuestCartService.GetCart(ctx, cs.Auth(ctx), guestcartid.(string))
 		if err != nil {
 			cs.Logger.Errorf("cart.application.cartservice: Guestcart id in session cannot be retrieved. Id %s, Error: %s", guestcartid, err)
 		}
+
 		return existingCart, err
 	}
+
 	return cart, errors.New("No cart in session yet")
 }
 
@@ -192,26 +200,29 @@ func (cs *CartService) GetSessionGuestCart(ctx web.Context) (domaincart.Cart, er
 func (cs *CartService) DeleteSessionGuestCart(ctx web.Context) error {
 	delete(ctx.Session().Values, "cart.guestid")
 	//TODO - trigger backend also to be able to delete the cart there ( cs.GuestCartService.DeleteCart())
+
 	return nil
 }
 
 // createNewSessionGuestCart Requests a new Guest Cart and stores the id in the session, if possible
-func (cs *CartService) createNewSessionGuestCart(ctx web.Context) (domaincart.Cart, error) {
+func (cs *CartService) createNewSessionGuestCart(ctx web.Context) (cartDomain.Cart, error) {
 	newGuestCart, err := cs.GuestCartService.GetNewCart(ctx, cs.Auth(ctx))
 	if err != nil {
 		cs.Logger.Errorf("cart.application.cartservice: Cannot create a new guest cart. Error %s", err)
 		delete(ctx.Session().Values, "cart.guestid")
+
 		return newGuestCart, err
 	}
 	cs.Logger.Infof("cart.application.cartservice: Requested new Guestcart %v", newGuestCart)
 	ctx.Session().Values["cart.guestid"] = newGuestCart.ID
+
 	return newGuestCart, nil
 }
 
-func (cs *CartService) getEmptyCart() (domaincart.Cart, error) {
-	return domaincart.Cart{}, nil
+func (cs *CartService) getEmptyCart() (cartDomain.Cart, error) {
+	return cartDomain.Cart{}, nil
 }
 
-func (cs *CartService) publishAddtoCartEvent(ctx web.Context, currentCart domaincart.Cart, addRequest domaincart.AddRequest) {
+func (cs *CartService) publishAddtoCartEvent(ctx web.Context, currentCart cartDomain.Cart, addRequest cartDomain.AddRequest) {
 	currentCart.EventPublisher.PublishAddToCartEvent(ctx, addRequest.MarketplaceCode, addRequest.VariantMarketplaceCode, addRequest.Qty)
 }
