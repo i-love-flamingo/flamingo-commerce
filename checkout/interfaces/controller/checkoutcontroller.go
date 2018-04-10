adc00a0b912ce42ef855b2971e683e0609506f0a
Joachim Adomeit <joachim.adomeit@aoe.com>
2018-04-10 13:45:36 +0200

    Payment Debug Message

--- checkout/interfaces/controller/checkoutcontroller.go
+++ checkout/interfaces/controller/checkoutcontroller.go
@@ -216,6 +216,8 @@ func (cc *CheckoutController) ProcessPaymentAction(ctx web.Context) web.Response
 
 	cartPayment, err := provider.ProcessPayment(ctx, paymentMethod, nil)
 
+	cc.Logger.Debugf("ProcessPayment Error: %s", err.Error())
+
 	if err != nil {
 		// Redirect to Checkout Start again
 		return cc.Redirect("checkout.start", nil)
