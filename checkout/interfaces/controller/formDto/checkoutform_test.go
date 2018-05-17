2ae74558108e2362e236f0fc1c3891e23e4d8e41
Flamingo <flamingo@aoe.com>
2018-05-17 17:17:17 +0200

    Bind to AOEpeople/conform commit a57993a078fec828d6dd95a7f61905e799986341

--- checkout/interfaces/controller/formDto/checkoutform_test.go
+++ checkout/interfaces/controller/formDto/checkoutform_test.go
@@ -22,13 +22,15 @@ func (f *FakeCustomer) GetAddresses() []domain.Address {
 func (f *FakeCustomer) GetDefaultShippingAddress() *domain.Address {
 	return &domain.Address{
 		Firstname: "first",
-		Email:     "mail",
+		//Email:     "example@mail.dmn",
+		Email: "mail",
 	}
 }
 func (f *FakeCustomer) GetDefaultBillingAddress() *domain.Address {
 	return &domain.Address{
 		Firstname: "first",
-		Email:     "mail",
+		//Email:     "example@mail.dmn",
+		Email: "mail",
 	}
 }
 
@@ -50,6 +52,7 @@ func TestFormService(t *testing.T) {
 	urlValues := make(map[string][]string)
 	form, err := service.ParseFormData(nil, urlValues)
 	form = service.GetDefaultFormData(form)
+
 	if checkoutForm, ok := form.(CheckoutFormData); ok {
 		if checkoutForm.BillingAddress.Email != "mail" {
 			t.Errorf("Wrong mail in data - expected to be initialized")
