2b913799e1bbb0423b50f4fa423a11fdb4273372
Thomas Neumann <thomas.neumann@aoe.com>
2018-07-05 10:29:23 +0200

    Minor refactorings and add receivers for DecoratedOrderItem

--- order/domain/orderDecorator.go
+++ order/domain/orderDecorator.go
@@ -2,6 +2,7 @@ package domain
 
 import (
 	"context"
+	"log"
 	"sort"
 
 	"flamingo.me/flamingo-commerce/product/domain"
@@ -10,7 +11,7 @@ import (
 
 type (
 	OrderDecoratorInterface interface {
-		Create(context.Context, *Order) *DecoratedOrder
+		Create(context.Context, Order) DecoratedOrder
 	}
 
 	// OrderDecorator
@@ -21,14 +22,14 @@ type (
 
 	// DecoratedOrder
 	DecoratedOrder struct {
-		Order          *Order
-		DecoratedItems []*DecoratedOrderItem
+		Order          Order
+		DecoratedItems []DecoratedOrderItem
 	}
 
 	// DecoratedOrderItem
 	DecoratedOrderItem struct {
-		Item    *OrderItem
-		Product *domain.BasicProduct
+		Item    OrderItem
+		Product domain.BasicProduct
 	}
 
 	//
@@ -39,25 +40,25 @@ type (
 )
 
 // Create creates a new decorated order
-func (rd *OrderDecorator) Create(ctx context.Context, order *Order) *DecoratedOrder {
-	result := &DecoratedOrder{Order: order}
+func (rd *OrderDecorator) Create(ctx context.Context, order Order) DecoratedOrder {
+	result := DecoratedOrder{Order: order}
 	result.DecoratedItems = rd.createDecoratedItems(ctx, order.OrderItems)
 
 	return result
 }
 
-func (rd *OrderDecorator) createDecoratedItems(ctx context.Context, items []OrderItem) []*DecoratedOrderItem {
-	result := make([]*DecoratedOrderItem, len(items))
+func (rd *OrderDecorator) createDecoratedItems(ctx context.Context, items []OrderItem) []DecoratedOrderItem {
+	result := make([]DecoratedOrderItem, len(items))
 	for i, item := range items {
-		decoratedItem := rd.createDecoratedItem(ctx, &item)
+		decoratedItem := rd.createDecoratedItem(ctx, item)
 		result[i] = decoratedItem
 	}
 
 	return result
 }
 
-func (rd *OrderDecorator) createDecoratedItem(ctx context.Context, item *OrderItem) *DecoratedOrderItem {
-	result := &DecoratedOrderItem{
+func (rd *OrderDecorator) createDecoratedItem(ctx context.Context, item OrderItem) DecoratedOrderItem {
+	result := DecoratedOrderItem{
 		Item: item,
 	}
 
@@ -81,12 +82,12 @@ func (rd *OrderDecorator) createDecoratedItem(ctx context.Context, item *OrderIt
 			product = rd.createFallbackProduct(item)
 		}
 	}
-	result.Product = &product
+	result.Product = product
 
 	return result
 }
 
-func (rd *OrderDecorator) createFallbackProduct(item *OrderItem) domain.SimpleProduct {
+func (rd *OrderDecorator) createFallbackProduct(item OrderItem) domain.SimpleProduct {
 	return domain.SimpleProduct{
 		BasicProductData: domain.BasicProductData{
 			Title: item.Name,
@@ -97,6 +98,60 @@ func (rd *OrderDecorator) createFallbackProduct(item *OrderItem) domain.SimplePr
 	}
 }
 
+// IsConfigurable - checks if current order item is a configurable product
+func (doi DecoratedOrderItem) IsConfigurable() bool {
+	return doi.Product.Type() == domain.TYPECONFIGURABLE_WITH_ACTIVE_VARIANT
+}
+
+// GetVariant getter
+func (doi DecoratedOrderItem) GetVariant() (*domain.Variant, error) {
+	return doi.Product.(domain.ConfigurableProductWithActiveVariant).Variant(doi.Item.VariantMarketplaceCode)
+}
+
+// GetDisplayTitle getter
+func (doi DecoratedOrderItem) GetDisplayTitle() string {
+	if doi.IsConfigurable() {
+		variant, e := doi.GetVariant()
+		if e != nil {
+			return "Error Getting Variant"
+		}
+		return variant.Title
+	}
+	return doi.Product.BaseData().Title
+}
+
+// GetDisplayMarketplaceCode getter
+func (doi DecoratedOrderItem) GetDisplayMarketplaceCode() string {
+	if doi.IsConfigurable() {
+		variant, e := doi.GetVariant()
+		if e != nil {
+			return "Error Getting Variant"
+		}
+		return variant.MarketPlaceCode
+	}
+	return doi.Product.BaseData().MarketPlaceCode
+}
+
+// GetVariantsVariationAttribute getter
+func (doi DecoratedOrderItem) GetVariantsVariationAttributes() domain.Attributes {
+	attributes := domain.Attributes{}
+	if doi.IsConfigurable() {
+		variant, _ := doi.GetVariant()
+
+		for _, attributeName := range doi.Product.(domain.ConfigurableProductWithActiveVariant).VariantVariationAttributes {
+			attributes[attributeName] = variant.BaseData().Attributes[attributeName]
+		}
+	}
+	log.Println(attributes)
+	return attributes
+}
+
+// GetVariantsVariationAttribute getter
+func (doi DecoratedOrderItem) GetVariantsVariationAttributeCodes() []string {
+	return doi.Product.(domain.ConfigurableProductWithActiveVariant).VariantVariationAttributes
+}
+
+// GetGroupedBy
 func (rd *DecoratedOrder) GetGroupedBy(group string, sortGroup bool) []*GroupedDecoratedOrderItem {
 	groupedItemsCollection := make(map[string]*GroupedDecoratedOrderItem)
 	var groupedItemKeys []string
@@ -105,7 +160,7 @@ func (rd *DecoratedOrder) GetGroupedBy(group string, sortGroup bool) []*GroupedD
 	for _, item := range rd.DecoratedItems {
 		switch group {
 		case "retailer_code":
-			groupKey = (*item.Product).BaseData().RetailerCode
+			groupKey = item.Product.BaseData().RetailerCode
 		default:
 			groupKey = "default"
 		}
@@ -119,7 +174,7 @@ func (rd *DecoratedOrder) GetGroupedBy(group string, sortGroup bool) []*GroupedD
 		}
 
 		groupedItemsEntry, _ := groupedItemsCollection[groupKey]
-		groupedItemsEntry.DecoratedItems = append(groupedItemsEntry.DecoratedItems, *item)
+		groupedItemsEntry.DecoratedItems = append(groupedItemsEntry.DecoratedItems, item)
 	}
 
 	if sortGroup {
