a8fff70a764e543bada1462c9c81293ff48b3f24
Thomas Layh <thomas.layh@aoe.com>
2018-07-24 11:38:23 +0200

    [TASK] #173515 add helper functions for split cart - add unit tests for helper functions

--- cart/domain/cart/cart_test.go
+++ cart/domain/cart/cart_test.go
@@ -20,3 +20,62 @@ func TestSimpleCartItem(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Equal(t, &cartItem, item)
 }
+
+func TestCart_HasDeliveryIntentArrival(t *testing.T) {
+	var cart = new(Cart)
+
+	cart.Cartitems = append(cart.Cartitems, getItemWithArrivalIntent())
+
+	resultArrival := cart.HasItemWithIntent("arrival")
+	assert.True(t, resultArrival)
+}
+
+func TestCart_HasDeliveryIntentDeparture(t *testing.T) {
+	var cart = new(Cart)
+
+	cart.Cartitems = append(cart.Cartitems, getItemWithDepartureIntent())
+
+	resultDeparture := cart.HasItemWithIntent("departure")
+	assert.True(t, resultDeparture)
+}
+
+func TestCart_HasNoMixedCart(t *testing.T) {
+	var cart = new(Cart)
+
+	cart.Cartitems = append(cart.Cartitems, getItemWithDepartureIntent())
+
+	resultNoMixedCart := cart.HasMixedCart()
+	assert.False(t, resultNoMixedCart)
+}
+
+func TestCart_HasMixedCart(t *testing.T) {
+	var cart = new(Cart)
+
+	cart.Cartitems = append(cart.Cartitems, getItemWithDepartureIntent())
+	cart.Cartitems = append(cart.Cartitems, getItemWithArrivalIntent())
+
+	resultMixedCart := cart.HasMixedCart()
+	assert.True(t, resultMixedCart)
+}
+
+func getItemWithDepartureIntent() Item {
+	deliveryIntentDeparture := DeliveryIntent{
+		Method: "departure",
+	}
+	return Item{
+		MarketplaceCode:        "codeDeparture",
+		OriginalDeliveryIntent: &deliveryIntentDeparture,
+		Qty: 1,
+	}
+}
+
+func getItemWithArrivalIntent() Item {
+	deliveryIntentArrival := DeliveryIntent{
+		Method: "arrival",
+	}
+	return Item{
+		MarketplaceCode:        "codeArrival",
+		OriginalDeliveryIntent: &deliveryIntentArrival,
+		Qty: 1,
+	}
+}
