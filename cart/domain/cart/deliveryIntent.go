7a14203bf2d50bca532192029619b6dcdd44f8de
Thomas Layh <thomas.layh@aoe.com>
2018-08-03 09:17:14 +0200

    Revert "[Task] #173514 - [BE] Save flight information for different journeys - make sure all items get handled"
    This reverts commit 47fcd727d96d903a98daf5e0b9c974521d1ed19e.

--- cart/domain/cart/deliveryIntent.go
+++ cart/domain/cart/deliveryIntent.go
@@ -39,19 +39,12 @@ type (
 func (dib *DefaultDeliveryInfoBuilder) BuildDeliveryInfoUpdateCommand(ctx web.Context, decoratedCart *DecoratedCart) ([]DeliveryInfoUpdateCommand, error) {
 	var updateCommands []DeliveryInfoUpdateCommand
 
-	// check for all items
-	cartItemsToHandle := make(map[string]Item)
-	for _, currentItem := range decoratedCart.Cart.Cartitems {
-		cartItemsToHandle[currentItem.ID] = currentItem
-	}
-
 	if decoratedCart.Cart.HasDeliveryInfos() {
 		for _, delInfo := range decoratedCart.Cart.DeliveryInfos {
 			currentlyAssignedItems := []string{}
 			for _, item := range decoratedCart.Cart.Cartitems {
-				if item.DeliveryInfoReference != nil && item.DeliveryInfoReference.ID == delInfo.ID {
+				if item.DeliveryInfoReference.ID == delInfo.ID {
 					currentlyAssignedItems = append(currentlyAssignedItems, item.ID)
-					delete(cartItemsToHandle, item.ID)
 				}
 			}
 			newDelInfo := delInfo
@@ -60,11 +53,7 @@ func (dib *DefaultDeliveryInfoBuilder) BuildDeliveryInfoUpdateCommand(ctx web.Co
 				AssignedItemIds: currentlyAssignedItems,
 			})
 		}
-
-		// check if all items are assigned or if there is still something to assign
-		if len(cartItemsToHandle) <= 0 {
-			return updateCommands, nil
-		}
+		return updateCommands, nil
 	}
 
 	//Else - There are no deliveryInfos on the cart. So we use the DeliveryIntent to build the initial commands
@@ -72,19 +61,15 @@ func (dib *DefaultDeliveryInfoBuilder) BuildDeliveryInfoUpdateCommand(ctx web.Co
 		if len(cartitems) < 1 {
 			continue
 		}
-
-		// only continue if cartItem is in cartItemsToHandle
-		if _, ok := cartItemsToHandle[cartitems[0].ID]; ok {
-			deliveryInfo := cartitems[0].OriginalDeliveryIntent.BuildDeliveryInfo()
-			itemIds := make([]string, 0)
-			for _, cartitem := range cartitems {
-				itemIds = append(itemIds, cartitem.ID)
-			}
-			updateCommands = append(updateCommands, DeliveryInfoUpdateCommand{
-				DeliveryInfo:    &deliveryInfo,
-				AssignedItemIds: itemIds,
-			})
+		deliveryInfo := cartitems[0].OriginalDeliveryIntent.BuildDeliveryInfo()
+		itemIds := make([]string, 0)
+		for _, cartitem := range cartitems {
+			itemIds = append(itemIds, cartitem.ID)
 		}
+		updateCommands = append(updateCommands, DeliveryInfoUpdateCommand{
+			DeliveryInfo:    &deliveryInfo,
+			AssignedItemIds: itemIds,
+		})
 	}
 	return updateCommands, nil
 }
