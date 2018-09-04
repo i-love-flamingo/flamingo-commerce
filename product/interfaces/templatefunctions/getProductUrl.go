36aa03a1eca3ce1a4b7518c7a14b1c6e834eb6f7
Daniel Pötzinger <daniel.poetzinger@aoe.com>
2018-09-04 12:07:52 +0200

    add logging for getProductUrl template func

--- product/interfaces/templatefunctions/getProductUrl.go
+++ product/interfaces/templatefunctions/getProductUrl.go
@@ -3,12 +3,14 @@ package templatefunctions
 import (
 	"flamingo.me/flamingo-commerce/product/application"
 	"flamingo.me/flamingo-commerce/product/domain"
+	"flamingo.me/flamingo/framework/flamingo"
 )
 
 type (
 	// GetProductUrl is exported as a template function
 	GetProductUrl struct {
 		UrlService *application.UrlService `inject:""`
+		Logger     flamingo.Logger         `inject:""`
 	}
 )
 
@@ -16,10 +18,12 @@ type (
 func (tf *GetProductUrl) Func() interface{} {
 	return func(p domain.BasicProduct) string {
 		if p == nil {
+			tf.Logger.WithField("category", "product").Warn("Called getPrpductUrl templatefunc without a product")
 			return ""
 		}
 		url, err := tf.UrlService.Get(p, "")
 		if err != nil {
+			tf.Logger.WithField("category", "product").Error(err)
 			return ""
 		}
 		return url
