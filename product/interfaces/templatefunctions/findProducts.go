f7011b56ac8c68cff32906e4b6283b9fa8146244
Joachim Adomeit <joachim.adomeit@aoe.com>
2018-09-11 16:40:26 +0200

    180320: [FEATURE] Filter and Facets

--- product/interfaces/templatefunctions/findProducts.go
+++ product/interfaces/templatefunctions/findProducts.go
@@ -3,8 +3,10 @@ package templatefunctions
 import (
 	"context"
 	"log"
+	"flamingo.me/flamingo/core/pugtemplate/pugjs"
 
 	productDomain "flamingo.me/flamingo-commerce/product/domain"
+	searchDomain "flamingo.me/flamingo-commerce/search/domain"
 )
 
 type (
@@ -15,12 +17,36 @@ type (
 )
 
 func (tf *FindProducts) Func(ctx context.Context) interface{} {
-	return func() []productDomain.BasicProduct {
-		products, e := tf.SearchService.Search(ctx, nil)
+	return func(filtersPug pugjs.Map, sortPug pugjs.Map) []productDomain.BasicProduct {
+		var filter []searchDomain.Filter
+
+		// use filtersPug as KeyValueFilter
+		for k, v := range filtersPug.Items {
+			if v, ok := v.(*pugjs.Array); ok {
+				var filterList []string
+				for _, item:= range v.Items() {
+					filterList = append(filterList, item.String())
+				}
+				filter = append(filter, searchDomain.NewKeyValueFilter(k.String(), filterList))
+			}
+			if v, ok := v.(pugjs.String); ok {
+				filter = append(filter, searchDomain.NewKeyValueFilter(k.String(), []string{v.String()}))
+			}
+		}
+
+		// use sortPug as SortFilter
+		for k, v := range sortPug.Items {
+			if v, ok := v.(pugjs.String); ok {
+				filter = append(filter, searchDomain.NewSortFilter(k.String(), v.String()))
+			}
+		}
+
+		products, e := tf.SearchService.Search(ctx, filter...)
 		if e != nil {
 			log.Printf("Error: product.interfaces.templatefunc %v", e)
 			return nil
 		}
+
 		return products.Hits
 	}
 }
