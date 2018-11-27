2f0290d1e49762ec8bcafc08a87c35cffad8361b
Thomas Layh <thomas.layh@aoe.com>
2018-11-27 13:26:12 +0100

    [Task] #187676 - [BE] Implement API controller for billing address

--- checkout/interfaces/controller/checkoutapicontroller.go
+++ checkout/interfaces/controller/checkoutapicontroller.go
@@ -3,6 +3,8 @@ package controller
 import (
 	"context"
 
+	formApplicationService "flamingo.me/flamingo/core/form/application"
+	formDomain "flamingo.me/flamingo/core/form/domain"
 	"flamingo.me/flamingo/framework/flamingo"
 	"flamingo.me/flamingo/framework/web"
 )
@@ -10,8 +12,9 @@ import (
 type (
 	// CheckoutAPIController for onepage checkout
 	CheckoutAPIController struct {
-		responder *web.Responder
-		logger    flamingo.Logger
+		responder   *web.Responder
+		logger      flamingo.Logger
+		formService formDomain.FormService
 	}
 
 	// JSONResult for ajax response
@@ -19,17 +22,38 @@ type (
 		Message     string
 		MessageCode string
 		Success     bool
+		FieldErrors map[string][]formDomain.Error
 	}
 )
 
 // Inject required dependencies
-func (cac *CheckoutAPIController) Inject(r *web.Responder, l flamingo.Logger) {
+func (cac *CheckoutAPIController) Inject(r *web.Responder, l flamingo.Logger, fs formDomain.FormService) {
 	cac.responder = r
 	cac.logger = l
+	cac.formService = fs
 }
 
-// UpdateShippingAddressAction - save or update the shipping address
-func (cac *CheckoutAPIController) UpdateShippingAddressAction(ctx context.Context, r *web.Request) web.Response {
+// SubmitBillingAddressAction - save or update the shipping address
+func (cac *CheckoutAPIController) SubmitBillingAddressAction(ctx context.Context, r *web.Request) web.Response {
+
+	form, err := formApplicationService.ProcessFormRequest(ctx, r, cac.formService)
+	if err != nil {
+		cac.logger.Error("Error processing form ", form)
+	}
+
+	if !form.IsValidAndSubmitted() {
+		return &web.JSONResponse{
+			BasicResponse: web.BasicResponse{
+				Status: 400,
+			},
+			Data: JSONResult{
+				Message:     "error parsing one ore more fields",
+				MessageCode: "form.invalid",
+				Success:     false,
+				FieldErrors: form.ValidationInfo.FieldErrors,
+			},
+		}
+	}
 
 	return &web.JSONResponse{
 		Data: JSONResult{
