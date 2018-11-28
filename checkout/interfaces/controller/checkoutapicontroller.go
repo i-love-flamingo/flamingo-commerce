60b96ac68a3c501e61221a594adb57e98ab4f00c
Thomas Layh <thomas.layh@aoe.com>
2018-11-28 16:35:55 +0100

    [Task] #188015 - [BE] Save billing address to cart in magento

--- checkout/interfaces/controller/checkoutapicontroller.go
+++ checkout/interfaces/controller/checkoutapicontroller.go
@@ -8,15 +8,16 @@ import (
 	formDomain "flamingo.me/flamingo/core/form/domain"
 	"flamingo.me/flamingo/framework/flamingo"
 	"flamingo.me/flamingo/framework/web"
+	...CheckoutDomain ".../.../flamingo/src/checkout/domain"
 )
 
 type (
 	// CheckoutAPIController for onepage checkout
 	CheckoutAPIController struct {
-		responder   *web.Responder
-		logger      flamingo.Logger
-		formService formDomain.FormService
-		cartService *application.CartService
+		responder           *web.Responder
+		logger              flamingo.Logger
+		formService         formDomain.FormService
+		cartReceiverService *application.CartReceiverService
 	}
 
 	// JSONResult for ajax response
@@ -29,10 +30,16 @@ type (
 )
 
 // Inject required dependencies
-func (cac *CheckoutAPIController) Inject(r *web.Responder, l flamingo.Logger, fs formDomain.FormService) {
+func (cac *CheckoutAPIController) Inject(
+	r *web.Responder,
+	l flamingo.Logger,
+	fs formDomain.FormService,
+	crs *application.CartReceiverService,
+) {
 	cac.responder = r
 	cac.logger = l
 	cac.formService = fs
+	cac.cartReceiverService = crs
 }
 
 // SubmitBillingAddressAction - save or update the shipping address
@@ -60,49 +67,42 @@ func (cac *CheckoutAPIController) SubmitBillingAddressAction(ctx context.Context
 	}
 
 	// submit valid data
-	/*
-		if billingFormData, ok := form.Data.(heckoutDomain.BillingAddressForm); ok {
-			billingAddress := billingFormData.MapBillingAddress(billingFormData.AddressFormData)
+	if billingFormData, ok := form.Data.(...CheckoutDomain.BillingAddressForm); ok {
+		billingAddress := billingFormData.MapBillingAddress(billingFormData.AddressFormData)
 
-			// @todo remove as soon as there is a real session
-			session := new(sessions.Session)
-			session.Values = make(map[interface{}]interface{})
-			session.Values["cart.guestid"] = "e7b657272d58de42a86bf4eea4d686e1"
-			// @todo end of todo
-
-			cart, cartOrderBehaviour, err := cac.cartService.CartReceiverService.GetCart(ctx, session)
-			if err != nil {
-				cac.logger.Error("no cart to update")
-				return &web.JSONResponse{
-					BasicResponse: web.BasicResponse{
-						Status: 400,
-					},
-					Data: JSONResult{
-						Message:     "error saving information to cart",
-						MessageCode: "submit.error",
-						Success:     false,
-						FieldErrors: nil,
-					},
-				}
+		cart, cartOrderBehaviour, err := cac.cartReceiverService.GetCart(ctx, r.Session().G())
+		if err != nil {
+			cac.logger.Error("no cart to update")
+			return &web.JSONResponse{
+				BasicResponse: web.BasicResponse{
+					Status: 400,
+				},
+				Data: JSONResult{
+					Message:     "error saving information to cart",
+					MessageCode: "submit.error",
+					Success:     false,
+					FieldErrors: nil,
+				},
 			}
-
-			_, cartUpdateErr := cartOrderBehaviour.UpdateBillingAddress(ctx, cart, billingAddress)
-			if cartUpdateErr != nil {
-				return &web.JSONResponse{
-					BasicResponse: web.BasicResponse{
-						Status: 400,
-					},
-					Data: JSONResult{
-						Message:     "error saving information to cart",
-						MessageCode: "submit.error",
-						Success:     false,
-						FieldErrors: nil,
-					},
-				}
-			}
-
 		}
-	*/
+
+		_, cartUpdateErr := cartOrderBehaviour.UpdateBillingAddress(ctx, cart, billingAddress)
+		if cartUpdateErr != nil {
+			cac.logger.Error(cartUpdateErr)
+			return &web.JSONResponse{
+				BasicResponse: web.BasicResponse{
+					Status: 400,
+				},
+				Data: JSONResult{
+					Message:     "error saving information to cart",
+					MessageCode: "submit.error",
+					Success:     false,
+					FieldErrors: nil,
+				},
+			}
+		}
+
+	}
 
 	return &web.JSONResponse{
 		Data: JSONResult{
