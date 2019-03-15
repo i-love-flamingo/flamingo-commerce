887b4c107f24d49b4edfc959f026aaa1142c1e34
Daniel Pötzinger <daniel.poetzinger@aoe.com>
2019-03-15 10:38:43 +0100

    cleanup

--- cart/interfaces/controller/form/updateDeliveryInfo.go
+++ cart/interfaces/controller/form/updateDeliveryInfo.go
@@ -3,17 +3,6 @@ package form
 import "time"
 
 type (
-	// TODO: this is temp moved code?
-	// CheckoutFormData defines the default form data provided by the checkout form
-	UpdateDeliveryInfoCommandDto struct {
-		Code    string `form:"code"`
-		Method  string `form:"method"`
-		Carrier string `form:"carrier"`
-		//DeliveryLocation DeliveryLocation
-		DesiredTime string `form:"desiredTime"`
-		//AdditionalData   map[string]string
-		RelatedFlight FlightData `form:"flightData"`
-	}
 
 	// FlightData value object
 	FlightData struct {
