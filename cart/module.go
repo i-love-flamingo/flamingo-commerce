i-love-flamingo/pugtemplate

rename all imports

module.go
@@ -2,18 +2,19 @@ package pugtemplate
 
 import (
 	"encoding/json"
-	"flamingo/core/pugtemplate/pugjs"
-	"flamingo/core/pugtemplate/templatefunctions"
-	"flamingo/framework/config"
-	"flamingo/framework/dingo"
-	"flamingo/framework/router"
-	"flamingo/framework/template"
-	"flamingo/framework/web"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"path/filepath"
 	"strings"
+
+	"go.aoe.com/flamingo/core/pugtemplate/pugjs"
+	"go.aoe.com/flamingo/core/pugtemplate/templatefunctions"
+	"go.aoe.com/flamingo/framework/config"
+	"go.aoe.com/flamingo/framework/dingo"
+	"go.aoe.com/flamingo/framework/router"
+	"go.aoe.com/flamingo/framework/template"
+	"go.aoe.com/flamingo/framework/web"
 )
 
 type (

i-love-flamingo/flamingo-commerce

cart/module.go
@@ -2,12 +2,14 @@ package cart
 
 import (
 	"encoding/gob"
-	"flamingo/core/cart/domain/cart"
-	"flamingo/core/cart/infrastructure"
-	controller "flamingo/core/cart/interfaces/controller"
-	"flamingo/framework/config"
-	"flamingo/framework/dingo"
-	"flamingo/framework/router"
+
+	controller "go.aoe.com/flamingo/core/cart/interfaces/controller"
+
+	"go.aoe.com/flamingo/core/cart/domain/cart"
+	"go.aoe.com/flamingo/core/cart/infrastructure"
+	"go.aoe.com/flamingo/framework/config"
+	"go.aoe.com/flamingo/framework/dingo"
+	"go.aoe.com/flamingo/framework/router"
 )
 
 type (