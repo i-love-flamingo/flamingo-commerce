6251902b10b9f8064f2604ee5b0ea1a76db36366
Stanislav Müller <stanislav.mueller@aoe.com>
2019-04-30 16:30:55 +0200

    T#198424 Implement price application service

---
+++ price/application/service.go
@@ -0,0 +1,62 @@
+package application
+
+import (
+	"flamingo.me/flamingo-commerce/v3/price/domain"
+
+	"flamingo.me/flamingo/v3/core/locale/application"
+	"flamingo.me/flamingo/v3/framework/config"
+	"github.com/leekchan/accounting"
+)
+
+// Service for formatting prices
+type Service struct {
+	config       config.Map
+	labelService *application.LabelService
+}
+
+// Inject dependencies
+func (s *Service) Inject(labelService *application.LabelService, config *struct {
+	Config config.Map `inject:"config:locale.accounting"`
+}) {
+	s.labelService = labelService
+	s.config = config.Config
+}
+
+// GetConfigForCurrency
+func (s *Service) GetConfigForCurrency(currency string) config.Map {
+	if configForCurrency, ok := s.config[currency]; ok {
+		return configForCurrency.(config.Map)
+	}
+
+	return s.config["default"].(config.Map)
+}
+
+// FormatPrice
+func (s *Service) FormatPrice(price domain.Price) string {
+	currency := s.labelService.NewLabel(price.Currency()).String()
+
+	configForCurrency := s.GetConfigForCurrency(price.Currency())
+
+	ac := accounting.Accounting{
+		Symbol:    currency,
+		Precision: 2,
+	}
+	decimal, ok := configForCurrency["decimal"].(string)
+	if ok {
+		ac.Decimal = decimal
+	}
+	thousand, ok := configForCurrency["thousand"].(string)
+	if ok {
+		ac.Thousand = thousand
+	}
+	formatZero, ok := configForCurrency["formatZero"].(string)
+	if ok {
+		ac.FormatZero = formatZero
+	}
+	format, ok := configForCurrency["format"].(string)
+	if ok {
+		ac.Format = format
+	}
+
+	return ac.FormatMoney(price.GetPayable().FloatAmount())
+}
