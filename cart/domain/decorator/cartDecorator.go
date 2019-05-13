341e61b1b554c92506e30a523abf51c3f8fa56a6
Carsten Dietrich <carsten.dietrich@aoe.com>
2019-05-13 12:54:12 +0200

    cart: Use row price net with discount

--- cart/domain/decorator/cartDecorator.go
+++ cart/domain/decorator/cartDecorator.go
@@ -171,8 +171,8 @@ func (dci DecoratedCartItem) GetVariantsVariationAttributeCodes() []string {
 
 // GetChargesToPay getter
 func (dci DecoratedCartItem) GetChargesToPay(wishedToPaySum *domain.WishedToPay) priceDomain.Charges {
-	price := dci.Item.RowPriceNet
-	return dci.Product.SaleableData().GetLoyaltyChargeSplit(&price, wishedToPaySum, dci.Item.Qty)
+	priceToPayForItem := dci.Item.RowPriceNetWithDiscount()
+	return dci.Product.SaleableData().GetLoyaltyChargeSplit(&priceToPayForItem, wishedToPaySum, dci.Item.Qty)
 }
 
 // GetGroupedBy legacy function
