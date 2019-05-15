299f560aff2c50aba921efd42dc47665b972f6d1
Joachim Adomeit <joachim.adomeit@aoe.com>
2019-05-15 15:46:39 +0200

    Adding Name to Restrictor Interface

--- cart/domain/validation/restrictionService.go
+++ cart/domain/validation/restrictionService.go
@@ -13,16 +13,20 @@ type (
 	RestrictionService struct {
 		qtyRestrictors []MaxQuantityRestrictor
 	}
+
 	// RestrictionResult contains the result of a restriction
 	RestrictionResult struct {
 		IsRestricted        bool
 		MaxAllowed          int
 		RemainingDifference int
+		RestrictorName      string
 	}
 
 	// MaxQuantityRestrictor returns the maximum qty allowed for a given product and cart
 	// it is possible to register many (MultiBind) MaxQuantityRestrictor implementations
 	MaxQuantityRestrictor interface {
+		// Name returns the code of the restrictor
+		Name() string
 		// Restrict must return a `RestrictionResult` which contains information regarding if a restriction is
 		// applied and whats the max allowed quantity
 		Restrict(ctx context.Context, product domain.BasicProduct, cart *cart.Cart) *RestrictionResult
@@ -43,8 +47,8 @@ func (rs *RestrictionService) Inject(
 func (rs *RestrictionService) RestrictQty(ctx context.Context, product domain.BasicProduct, currentCart *cart.Cart) *RestrictionResult {
 	restrictionResult := &RestrictionResult{
 		IsRestricted:        false,
-		RemainingDifference: math.MaxInt32,
 		MaxAllowed:          math.MaxInt32,
+		RemainingDifference: math.MaxInt32,
 	}
 
 	for _, r := range rs.qtyRestrictors {
@@ -55,6 +59,7 @@ func (rs *RestrictionService) RestrictQty(ctx context.Context, product domain.Ba
 
 			if currentResult.MaxAllowed < restrictionResult.MaxAllowed {
 				restrictionResult.MaxAllowed = currentResult.MaxAllowed
+				restrictionResult.RestrictorName = currentResult.RestrictorName
 			}
 
 			if currentResult.RemainingDifference < restrictionResult.RemainingDifference {
