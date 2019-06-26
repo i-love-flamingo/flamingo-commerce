10fc22044b942eb028d798c74a241c8860a64b48
Daniel Pötzinger <daniel.poetzinger@aoe.com>
2019-06-26 13:47:18 +0200

    add test

--- checkout/domain/sourcingengine_test.go
+++ checkout/domain/sourcingengine_test.go
@@ -59,3 +59,34 @@ func TestSources_Reduce(t *testing.T) {
 	sources = sources.Reduce(sourcesreduce)
 	assert.Equal(t, 2, sources.QtySum())
 }
+
+
+func TestSources_ReduceInNeg(t *testing.T) {
+	var sources Sources
+	assert.Equal(t, "", sources.MainLocation())
+
+	sources = append(sources, Source{
+		LocationCode: "loc1",
+		Qty:          1,
+	})
+
+	sources = append(sources, Source{
+		LocationCode: "loc2",
+		Qty:          2,
+	})
+
+	var sourcesreduce1 Sources
+	sourcesreduce1 = append(sourcesreduce1, Source{
+		LocationCode: "loc2",
+		Qty:          5,
+	})
+	var sourcesreduce2 Sources
+	sourcesreduce2 = append(sourcesreduce2, Source{
+		LocationCode: "loc2",
+		Qty:          2,
+	})
+
+	sources = sources.Reduce(sourcesreduce1)
+	sources = sources.Reduce(sourcesreduce2)
+	assert.Equal(t, -4, sources.QtySum())
+}
