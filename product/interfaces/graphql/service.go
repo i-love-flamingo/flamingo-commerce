0fe223382068e532a4d79fb2254595a7adea8e50
Leo Back <leo.back@aoe.com>
2019-06-26 16:10:21 +0200

    [Task] #201908 - Create product schema & resolver

--- product/interfaces/graphql/service.go
+++ product/interfaces/graphql/service.go
@@ -11,128 +11,7 @@ import (
 type Service struct{}
 
 func (*Service) Schema() []byte {
-	// language=graphql
-	return []byte(`
-interface Commerce_Product {
-	baseData: Commerce_BasicProductData!
-	teaserData: Commerce_ProductTeaserData!
-	specifications: Commerce_ProductSpecifications!
-	isSaleable: Boolean!
-	saleableData: Commerce_ProductSaleable!
-	type: String!
-	getIdentifier: String!
-	hasMedia(group: String!, usage: String!): Boolean!
-	getMedia(group: String!, usage: String!): Commerce_ProductMedia!
-}
-
-type Commerce_SimpleProduct implements Commerce_Product {
-	baseData: Commerce_BasicProductData!
-	teaserData: Commerce_ProductTeaserData!
-	specifications: Commerce_ProductSpecifications!
-	isSaleable: Boolean!
-	saleableData: Commerce_ProductSaleable!
-	type: String!
-	getIdentifier: String!
-	hasMedia(group: String!, usage: String!): Boolean!
-	getMedia(group: String!, usage: String!): Commerce_ProductMedia!
-}
-
-type Commerce_BasicProductData {
-	title:            String!
-	attributes:       Commerce_ProductAttributes
-	shortDescription: String!
-	description:      String!
-	media:            [Commerce_ProductMedia!]
-
-	marketPlaceCode: String!
-	retailerCode:    String!
-	retailerSku:     String!
-	retailerName:    String!
-
-	createdAt:   Time!
-	updatedAt:   Time!
-	visibleFrom: Time!
-	visibleTo:   Time!
-
-#	Categories:   [Commerce_CategoryTeaser!]
-#	MainCategory: [Commerce_CategoryTeaser!]
-
-	categoryToCodeMapping: [String!]
-
-	stockLevel: String!
-
-	keywords: [String!]
-	isNew:    Boolean!
-}
-
-type Commerce_ProductTeaserData {
-	shortTitle: String
-	shortDescription: String
-	# TeaserPrice is the price that should be shown in teasers (listview)
-	# teaserPrice : Commerce_ProductPriceInfo
-	# TeaserPriceIsFromPrice - is set to true in cases where a product might have different prices (e.g. configurable)
-	teaserPriceIsFromPrice: Boolean
-	# PreSelectedVariantSku - might be set for configurables to give a hint to link to a variant of a configurable (That might be the case if a user filters for an attribute and in the teaser the variant with that attribute is shown)
-	preSelectedVariantSku: String
-	# Media
- 	media : [Commerce_ProductMedia!]
-	# The sku that should be used to link from Teasers
-	marketPlaceCode: String
-	#teaserAvailablePrices : [PriceInfo!]
-	# TeaserLoyaltyPriceInfo - optional the Loyaltyprice that can be used for teaser (e.g. on listing views)
-	#teaserLoyaltyPriceInfo *LoyaltyPriceInfo
-}
-
-type Commerce_ProductSpecifications {
-	groups: [Commerce_ProductSpecificationGroup!]
-}
-
-type Commerce_ProductSpecificationGroup {
-	title: String!
-	entries: [Commerce_ProductSpecificationEntry!]
-}
-
-type Commerce_ProductSpecificationEntry {
-	label: String!
-	values: [String!]
-}
-
-type Commerce_ProductSaleable {
-	isSaleable: Boolean!
-	saleableFrom: Time
-	saleableTo: Time
-#	activePrice: Commerce_ProductPriceInfo
-#	availablePrices: [Commerce_ProductPriceInfo!]
-	# loyaltyPrices - Optional infos for products that can be payed in a loyalty program
-#	loyaltyPrices: [Commerce_ProductLoyaltyPriceInfo!]
-}
-
-type Commerce_ProductMedia {
-	type:      String!
-	mimeType:  String!
-	usage:     String!
-	title:     String!
-	reference: String!
-}
-
-type Commerce_ProductAttributes {
-	getAttributeKeys: [String!]
-	getAttributes: [Commerce_ProductAttribute!]
-	hasAttribute(key: String!): Boolean
-	getAttribute(key: String!): Commerce_ProductAttribute
-}
-
-type Commerce_ProductAttribute {
-	code: String!
-	label: String!
-#	rawValue: String!
-	unitCode: String!
-}
-
-extend type Query {
-	Commerce_Product(marketplaceCode: String!): Commerce_Product
-}
-`)
+	return MustAsset("schema.graphql")
 }
 
 func (*Service) Models() map[string]config.TypeMapEntry {
@@ -158,6 +37,7 @@ func (*Service) Models() map[string]config.TypeMapEntry {
 		"Commerce_ProductMedia":              domain.Media{},
 		"Commerce_ProductAttributes":         domain.Attributes{},
 		"Commerce_ProductAttribute":          domain.Attribute{},
+		"Commerce_CategoryTeaser":            domain.CategoryTeaser{},
 	}.Models()
 }
 
