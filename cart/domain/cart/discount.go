6f2b43747d91470a7ec698f720e287053d860e80
joerg.einfeldt <joerg.einfeldt@aoe.com>
2019-07-08 11:10:29 +0200

    Add item discounts

--- cart/domain/cart/discount.go
+++ cart/domain/cart/discount.go
@@ -16,24 +16,27 @@ type (
 	// WithDiscount interface for a cart that is able to handle discounts
 	WithDiscount interface {
 		HasAppliedDiscounts() bool
-		AggregateDiscounts() []*AppliedDiscount
+		CollectDiscounts() []*AppliedDiscount
 	}
+
+	// ByCode implements sort.Interface for []AppliedDiscount based on code
+	ByCode []*AppliedDiscount
 )
 
-// AggregateDiscounts sums up discounts of cart based on its deliveries
+// CollectDiscounts sums up discounts of cart based on its deliveries
 // All discounts with the same type and title are aggregated and returned as one with a summed price
-func (c *Cart) AggregateDiscounts() []*AppliedDiscount {
+func (c *Cart) CollectDiscounts() []*AppliedDiscount {
 	return nil
 }
 
 // HasAppliedDiscounts check whether there are any discounts currently applied to the cart
 func (c *Cart) HasAppliedDiscounts() bool {
-	return len(c.AggregateDiscounts()) > 0
+	return len(c.CollectDiscounts()) > 0
 }
 
-// AggregateDiscounts sums up discounts of a delivery based on its single item discounts
+// CollectDiscounts sums up discounts of a delivery based on its single item discounts
 // All discounts with the same type and title are aggregated and returned as one with a summed price
-func (d *Delivery) AggregateDiscounts() []*AppliedDiscount {
+func (d *Delivery) CollectDiscounts() []*AppliedDiscount {
 	result := make([]*AppliedDiscount, 0)
 	// guard if no items in delivery, no need to iterate
 	if len(d.Cartitems) <= 0 {
@@ -44,40 +47,44 @@ func (d *Delivery) AggregateDiscounts() []*AppliedDiscount {
 
 // HasAppliedDiscounts check whether there are any discounts currently applied to the delivery
 func (d *Delivery) HasAppliedDiscounts() bool {
-	return len(d.AggregateDiscounts()) > 0
+	return len(d.CollectDiscounts()) > 0
 }
 
-// AggregateDiscounts parses discounts of a single item
+// CollectDiscounts parses discounts of a single item
 // All discounts with the same type and title are aggregated and returned as one with a summed price
-func (i *Item) AggregateDiscounts() []*AppliedDiscount {
-	result := make([]*AppliedDiscount, 0)
+func (i *Item) CollectDiscounts() []*AppliedDiscount {
 	// guard if no items in delivery, no need to iterate
 	if len(i.AppliedDiscounts) <= 0 {
-		return result
+		return make([]*AppliedDiscount, 0)
 	}
-	// aggregate discounts by type + title
-	collection := make(map[string]*AppliedDiscount)
-	for _, discount := range i.AppliedDiscounts {
-		key := discount.Type + discount.Title
-		if collected, ok := collection[key]; ok {
-			collected.Applied.Add(discount.Amount)
-			break
+	// parse item discounts to applied discounts
+	result := make([]*AppliedDiscount, len(i.AppliedDiscounts))
+	for index, val := range i.AppliedDiscounts {
+		result[index] = &AppliedDiscount{
+			Code:    val.Code,
+			Title:   val.Title,
+			Applied: val.Amount,
+			Type:    val.Type,
 		}
-		collection[key] = &AppliedDiscount{
-			Code:    discount.Code,
-			Title:   discount.Title,
-			Applied: discount.Amount,
-			Type:    discount.Type,
-		}
-	}
-	// restructure map to slice
-	for _, val := range collection {
-		result = append(result, val)
 	}
 	return result
 }
 
 // HasAppliedDiscounts check whether there are any discounts currently applied to the cart
 func (i *Item) HasAppliedDiscounts() bool {
-	return len(i.AggregateDiscounts()) > 0
+	return len(i.CollectDiscounts()) > 0
+}
+
+// implementations for sort interface
+
+func (a ByCode) Len() int {
+	return len(a)
+}
+
+func (a ByCode) Swap(i, j int) {
+	a[i], a[j] = a[j], a[i]
+}
+
+func (a ByCode) Less(i, j int) bool {
+	return a[i].Code < a[j].Code
 }
---
+++ cart/domain/cart/discount_test.go
@@ -0,0 +1,74 @@
+package cart_test
+
+import (
+	"reflect"
+	"sort"
+	"testing"
+
+	"flamingo.me/flamingo-commerce/v3/cart/domain/cart"
+)
+
+func TestItem_CollectDiscounts(t *testing.T) {
+	tests := []struct {
+		name string
+		item *cart.Item
+		want []*cart.AppliedDiscount
+	}{
+		{
+			name: "no discounts on item",
+			item: &cart.Item{},
+			want: []*cart.AppliedDiscount{},
+		},
+		{
+			name: "multiple different discounts on item",
+			item: func() *cart.Item {
+				builder := cart.ItemBuilder{}
+				builder.AddDiscount(cart.ItemDiscount{
+					Code:  "code-1",
+					Title: "title-1",
+					Type:  "type-1",
+				})
+				builder.AddDiscount(cart.ItemDiscount{
+					Code:  "code-1",
+					Title: "title-2",
+					Type:  "type-1",
+				})
+				builder.AddDiscount(cart.ItemDiscount{
+					Code:  "code-1",
+					Title: "title-1",
+					Type:  "type-2",
+				})
+				item, _ := builder.Build()
+				return item
+			}(),
+			want: []*cart.AppliedDiscount{
+				{
+					Code:  "code-1",
+					Title: "title-1",
+					Type:  "type-1",
+				},
+				{
+					Code:  "code-1",
+					Title: "title-2",
+					Type:  "type-1",
+				},
+				{
+					Code:  "code-1",
+					Title: "title-1",
+					Type:  "type-2",
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.item.CollectDiscounts()
+			// we need to sort result to circumvent implementation changes in order
+			sort.Sort(cart.ByCode(got))
+			sort.Sort(cart.ByCode(tt.want))
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Cart.CollectDiscounts() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
