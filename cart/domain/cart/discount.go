1ddb74183d1329c8e90d3ac4d50bd3d09850bdb0
joerg.einfeldt <joerg.einfeldt@aoe.com>
2019-07-08 14:40:30 +0200

    cart: pull discounts from item to cart level and merge accordingly

--- cart/domain/cart/discount.go
+++ cart/domain/cart/discount.go
@@ -15,8 +15,8 @@ type (
 
 	// WithDiscount interface for a cart that is able to handle discounts
 	WithDiscount interface {
-		HasAppliedDiscounts() bool
-		CollectDiscounts() []*AppliedDiscount
+		HasAppliedDiscounts() (bool, error)
+		CollectDiscounts() ([]*AppliedDiscount, error)
 	}
 
 	// ByCode implements sort.Interface for []AppliedDiscount based on code
@@ -25,37 +25,68 @@ type (
 
 // CollectDiscounts sums up discounts of cart based on its deliveries
 // All discounts with the same type and title are aggregated and returned as one with a summed price
-func (c *Cart) CollectDiscounts() []*AppliedDiscount {
-	return nil
+func (c *Cart) CollectDiscounts() ([]*AppliedDiscount, error) {
+	// guard if no items in delivery, no need to iterate
+	if len(c.Deliveries) <= 0 {
+		return make([]*AppliedDiscount, 0), nil
+	}
+	// collect different discounts on item level
+	var err error
+	collection := make(map[string]*AppliedDiscount)
+	for _, delivery := range c.Deliveries {
+		collection, err = mapDiscounts(collection, &delivery)
+		if err != nil {
+			return nil, err
+		}
+	}
+	// transform map to flat slice
+	return mapToSlice(collection), nil
 }
 
 // HasAppliedDiscounts check whether there are any discounts currently applied to the cart
-func (c *Cart) HasAppliedDiscounts() bool {
-	return len(c.CollectDiscounts()) > 0
+func (c *Cart) HasAppliedDiscounts() (bool, error) {
+	discounts, err := c.CollectDiscounts()
+	if err != nil {
+		return false, err
+	}
+	return len(discounts) > 0, nil
 }
 
 // CollectDiscounts sums up discounts of a delivery based on its single item discounts
 // All discounts with the same type and title are aggregated and returned as one with a summed price
-func (d *Delivery) CollectDiscounts() []*AppliedDiscount {
-	result := make([]*AppliedDiscount, 0)
+func (d *Delivery) CollectDiscounts() ([]*AppliedDiscount, error) {
 	// guard if no items in delivery, no need to iterate
 	if len(d.Cartitems) <= 0 {
-		return result
+		return make([]*AppliedDiscount, 0), nil
 	}
-	return result
+	// collect different discounts on item level
+	var err error
+	collection := make(map[string]*AppliedDiscount)
+	for _, item := range d.Cartitems {
+		collection, err = mapDiscounts(collection, &item)
+		if err != nil {
+			return nil, err
+		}
+	}
+	// transform map to flat slice
+	return mapToSlice(collection), nil
 }
 
 // HasAppliedDiscounts check whether there are any discounts currently applied to the delivery
-func (d *Delivery) HasAppliedDiscounts() bool {
-	return len(d.CollectDiscounts()) > 0
+func (d *Delivery) HasAppliedDiscounts() (bool, error) {
+	discounts, err := d.CollectDiscounts()
+	if err != nil {
+		return false, err
+	}
+	return len(discounts) > 0, nil
 }
 
 // CollectDiscounts parses discounts of a single item
 // All discounts with the same type and title are aggregated and returned as one with a summed price
-func (i *Item) CollectDiscounts() []*AppliedDiscount {
+func (i *Item) CollectDiscounts() ([]*AppliedDiscount, error) {
 	// guard if no items in delivery, no need to iterate
 	if len(i.AppliedDiscounts) <= 0 {
-		return make([]*AppliedDiscount, 0)
+		return make([]*AppliedDiscount, 0), nil
 	}
 	// parse item discounts to applied discounts
 	result := make([]*AppliedDiscount, len(i.AppliedDiscounts))
@@ -67,12 +98,56 @@ func (i *Item) CollectDiscounts() []*AppliedDiscount {
 			Type:    val.Type,
 		}
 	}
-	return result
+	return result, nil
 }
 
 // HasAppliedDiscounts check whether there are any discounts currently applied to the cart
-func (i *Item) HasAppliedDiscounts() bool {
-	return len(i.CollectDiscounts()) > 0
+func (i *Item) HasAppliedDiscounts() (bool, error) {
+	discounts, err := i.CollectDiscounts()
+	if err != nil {
+		return false, err
+	}
+	return len(discounts) > 0, nil
+}
+
+// private helper functions
+
+// mapToSlice transform map of discounts to flat slice
+func mapToSlice(collection map[string]*AppliedDiscount) []*AppliedDiscount {
+	result := make([]*AppliedDiscount, 0, len(collection))
+	for _, val := range collection {
+		result = append(result, val)
+	}
+	return result
+}
+
+// mapDiscounts map type + title of discount to corresponding discount
+func mapDiscounts(result map[string]*AppliedDiscount, discountable WithDiscount) (map[string]*AppliedDiscount, error) {
+	discounts, err := discountable.CollectDiscounts()
+	// in case discounts cannot be collected, stop execution
+	if err != nil {
+		return nil, err
+	}
+	for _, discount := range discounts {
+		key := discount.Type + discount.Title
+		// discount has been collected before, increase amount
+		if collected, ok := result[key]; ok {
+			update, err := collected.Applied.Add(discount.Applied)
+			if err != nil {
+				return nil, err
+			}
+			collected.Applied = update
+			continue
+		}
+		// discount is new, add to collection
+		result[key] = &AppliedDiscount{
+			Code:    discount.Code,
+			Title:   discount.Title,
+			Applied: discount.Applied,
+			Type:    discount.Type,
+		}
+	}
+	return result, nil
 }
 
 // implementations for sort interface
--- cart/domain/cart/discount_test.go
+++ cart/domain/cart/discount_test.go
@@ -6,7 +6,311 @@ import (
 	"testing"
 
 	"flamingo.me/flamingo-commerce/v3/cart/domain/cart"
+	"flamingo.me/flamingo-commerce/v3/price/domain"
 )
+
+// buildItemWithDiscounts helper for item building
+func buildItemWithDiscounts() *cart.Item {
+	builder := cart.ItemBuilder{}
+	builder.AddDiscount(cart.ItemDiscount{
+		Code:   "code-1",
+		Title:  "title-1",
+		Type:   "type-1",
+		Amount: domain.NewFromFloat(10.0, "$"),
+	})
+	builder.AddDiscount(cart.ItemDiscount{
+		Code:   "code-2",
+		Title:  "title-2",
+		Type:   "type-1",
+		Amount: domain.NewFromFloat(15.0, "$"),
+	})
+	builder.AddDiscount(cart.ItemDiscount{
+		Code:   "code-3",
+		Title:  "title-1",
+		Type:   "type-2",
+		Amount: domain.NewFromFloat(5.0, "$"),
+	})
+	item, _ := builder.Build()
+	return item
+}
+
+// buildDeliverxWithDiscounts helper for delivery building
+// Adds an item with discount twice
+// This means when discounts are summed up (based on type + delivery)
+// The amount should be added to the previous discount
+func buildDeliveryWithDiscounts() *cart.Delivery {
+	builder := cart.DeliveryBuilder{}
+	builder.SetDeliveryCode("code")
+	builder.AddItem(*buildItemWithDiscounts())
+	builder.AddItem(*buildItemWithDiscounts())
+	// add items with discounts
+	delivery, _ := builder.Build()
+	return delivery
+}
+
+func TestCart_CollectDiscounts(t *testing.T) {
+	tests := []struct {
+		name string
+		cart *cart.Cart
+		want []*cart.AppliedDiscount
+	}{
+		{
+			name: "empty cart",
+			cart: &cart.Cart{},
+			want: []*cart.AppliedDiscount{},
+		},
+		{
+			name: "cart with deliveries but without items",
+			cart: &cart.Cart{
+				Deliveries: func() []cart.Delivery {
+					result := make([]cart.Delivery, 0)
+					builder := cart.DeliveryBuilder{}
+					builder.SetDeliveryCode("code-1")
+					delivery, _ := builder.Build()
+					result = append(result, *delivery)
+					builder = cart.DeliveryBuilder{}
+					builder.SetDeliveryCode("code-2")
+					result = append(result, *delivery)
+					return result
+				}(),
+			},
+			want: []*cart.AppliedDiscount{},
+		},
+		{
+			name: "cart with deliveries with items but without discounts",
+			cart: &cart.Cart{
+				Deliveries: func() []cart.Delivery {
+					result := make([]cart.Delivery, 0)
+					builder := cart.DeliveryBuilder{}
+					builder.SetDeliveryCode("code-1")
+					builder.AddItem(cart.Item{})
+					builder.AddItem(cart.Item{})
+					delivery, _ := builder.Build()
+					result = append(result, *delivery)
+					builder = cart.DeliveryBuilder{}
+					builder.SetDeliveryCode("code-2")
+					result = append(result, *delivery)
+					return result
+				}(),
+			},
+			want: []*cart.AppliedDiscount{},
+		},
+		{
+			name: "cart with deliveries with items with discounts",
+			cart: &cart.Cart{
+				Deliveries: func() []cart.Delivery {
+					result := make([]cart.Delivery, 0)
+					delivery := buildDeliveryWithDiscounts()
+					result = append(result, *delivery)
+					delivery = buildDeliveryWithDiscounts()
+					result = append(result, *delivery)
+					return result
+				}(),
+			},
+			want: []*cart.AppliedDiscount{
+				{
+					Code:    "code-1",
+					Title:   "title-1",
+					Type:    "type-1",
+					Applied: domain.NewFromFloat(40.0, "$"),
+				},
+				{
+					Code:    "code-2",
+					Title:   "title-2",
+					Type:    "type-1",
+					Applied: domain.NewFromFloat(60.0, "$"),
+				},
+				{
+					Code:    "code-3",
+					Title:   "title-1",
+					Type:    "type-2",
+					Applied: domain.NewFromFloat(20.0, "$"),
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, _ := tt.cart.CollectDiscounts()
+			// we need to sort result to circumvent implementation changes in order
+			sort.Sort(cart.ByCode(got))
+			sort.Sort(cart.ByCode(tt.want))
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Cart.CollectDiscounts() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestCart_HasDiscounts(t *testing.T) {
+	tests := []struct {
+		name string
+		cart *cart.Cart
+		want bool
+	}{
+		{
+			name: "empty cart",
+			cart: &cart.Cart{},
+			want: false,
+		},
+		{
+			name: "cart with deliveries but without items",
+			cart: &cart.Cart{
+				Deliveries: func() []cart.Delivery {
+					result := make([]cart.Delivery, 0)
+					builder := cart.DeliveryBuilder{}
+					builder.SetDeliveryCode("code-1")
+					delivery, _ := builder.Build()
+					result = append(result, *delivery)
+					builder = cart.DeliveryBuilder{}
+					builder.SetDeliveryCode("code-2")
+					result = append(result, *delivery)
+					return result
+				}(),
+			},
+			want: false,
+		},
+		{
+			name: "cart with deliveries with items but without discounts",
+			cart: &cart.Cart{
+				Deliveries: func() []cart.Delivery {
+					result := make([]cart.Delivery, 0)
+					builder := cart.DeliveryBuilder{}
+					builder.SetDeliveryCode("code-1")
+					builder.AddItem(cart.Item{})
+					builder.AddItem(cart.Item{})
+					delivery, _ := builder.Build()
+					result = append(result, *delivery)
+					builder = cart.DeliveryBuilder{}
+					builder.SetDeliveryCode("code-2")
+					result = append(result, *delivery)
+					return result
+				}(),
+			},
+			want: false,
+		},
+		{
+			name: "cart with deliveries with items with discounts",
+			cart: &cart.Cart{
+				Deliveries: func() []cart.Delivery {
+					result := make([]cart.Delivery, 0)
+					delivery := buildDeliveryWithDiscounts()
+					result = append(result, *delivery)
+					delivery = buildDeliveryWithDiscounts()
+					result = append(result, *delivery)
+					return result
+				}(),
+			},
+			want: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got, _ := tt.cart.HasAppliedDiscounts(); got != tt.want {
+				t.Errorf("Cart.HasDiscounts() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestDelivery_CollectDiscounts(t *testing.T) {
+	tests := []struct {
+		name     string
+		delivery *cart.Delivery
+		want     []*cart.AppliedDiscount
+	}{
+		{
+			name:     "empty delivery",
+			delivery: &cart.Delivery{},
+			want:     []*cart.AppliedDiscount{},
+		},
+		{
+			name: "delivery with items but without discounts",
+			delivery: func() *cart.Delivery {
+				builder := cart.DeliveryBuilder{}
+				builder.AddItem(cart.Item{})
+				builder.AddItem(cart.Item{})
+				builder.SetDeliveryCode("code")
+				delivery, _ := builder.Build()
+				return delivery
+			}(),
+			want: []*cart.AppliedDiscount{},
+		},
+		{
+			name:     "delivery with items with discounts",
+			delivery: buildDeliveryWithDiscounts(),
+			want: []*cart.AppliedDiscount{
+				{
+					Code:    "code-1",
+					Title:   "title-1",
+					Type:    "type-1",
+					Applied: domain.NewFromFloat(20.0, "$"),
+				},
+				{
+					Code:    "code-2",
+					Title:   "title-2",
+					Type:    "type-1",
+					Applied: domain.NewFromFloat(30.0, "$"),
+				},
+				{
+					Code:    "code-3",
+					Title:   "title-1",
+					Type:    "type-2",
+					Applied: domain.NewFromFloat(10.0, "$"),
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, _ := tt.delivery.CollectDiscounts()
+			// we need to sort result to circumvent implementation changes in order
+			sort.Sort(cart.ByCode(got))
+			sort.Sort(cart.ByCode(tt.want))
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Delivery.CollectDiscounts() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestDelivery_HasDiscounts(t *testing.T) {
+	tests := []struct {
+		name     string
+		delivery *cart.Delivery
+		want     bool
+	}{
+		{
+			name:     "empty delivery",
+			delivery: &cart.Delivery{},
+			want:     false,
+		},
+		{
+			name: "delivery with items but without discounts",
+			delivery: func() *cart.Delivery {
+				builder := cart.DeliveryBuilder{}
+				builder.AddItem(cart.Item{})
+				builder.AddItem(cart.Item{})
+				builder.SetDeliveryCode("code")
+				delivery, _ := builder.Build()
+				return delivery
+			}(),
+			want: false,
+		},
+		{
+			name:     "delivery with items with discounts",
+			delivery: buildDeliveryWithDiscounts(),
+			want:     true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got, _ := tt.delivery.HasAppliedDiscounts(); got != tt.want {
+				t.Errorf("Delivery.HasDiscounts() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
 
 func TestItem_CollectDiscounts(t *testing.T) {
 	tests := []struct {
@@ -21,48 +325,32 @@ func TestItem_CollectDiscounts(t *testing.T) {
 		},
 		{
 			name: "multiple different discounts on item",
-			item: func() *cart.Item {
-				builder := cart.ItemBuilder{}
-				builder.AddDiscount(cart.ItemDiscount{
-					Code:  "code-1",
-					Title: "title-1",
-					Type:  "type-1",
-				})
-				builder.AddDiscount(cart.ItemDiscount{
-					Code:  "code-1",
-					Title: "title-2",
-					Type:  "type-1",
-				})
-				builder.AddDiscount(cart.ItemDiscount{
-					Code:  "code-1",
-					Title: "title-1",
-					Type:  "type-2",
-				})
-				item, _ := builder.Build()
-				return item
-			}(),
+			item: buildItemWithDiscounts(),
 			want: []*cart.AppliedDiscount{
 				{
-					Code:  "code-1",
-					Title: "title-1",
-					Type:  "type-1",
+					Code:    "code-1",
+					Title:   "title-1",
+					Type:    "type-1",
+					Applied: domain.NewFromFloat(10.0, "$"),
 				},
 				{
-					Code:  "code-1",
-					Title: "title-2",
-					Type:  "type-1",
+					Code:    "code-2",
+					Title:   "title-2",
+					Type:    "type-1",
+					Applied: domain.NewFromFloat(15.0, "$"),
 				},
 				{
-					Code:  "code-1",
-					Title: "title-1",
-					Type:  "type-2",
+					Code:    "code-3",
+					Title:   "title-1",
+					Type:    "type-2",
+					Applied: domain.NewFromFloat(5.0, "$"),
 				},
 			},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got := tt.item.CollectDiscounts()
+			got, _ := tt.item.CollectDiscounts()
 			// we need to sort result to circumvent implementation changes in order
 			sort.Sort(cart.ByCode(got))
 			sort.Sort(cart.ByCode(tt.want))
@@ -101,7 +389,7 @@ func TestItem_HasDiscounts(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := tt.item.HasAppliedDiscounts(); got != tt.want {
+			if got, _ := tt.item.HasAppliedDiscounts(); got != tt.want {
 				t.Errorf("Item.HasDiscounts() = %v, want %v", got, tt.want)
 			}
 		})
