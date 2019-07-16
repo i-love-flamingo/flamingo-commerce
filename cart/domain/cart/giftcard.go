d6f81b1848dd5e9fd2d96c21fa89802ca7ca06ea
joerg.einfeldt <joerg.einfeldt@aoe.com>
2019-07-16 17:33:57 +0200

    Enhance tests for giftcard behaviour

--- cart/domain/cart/giftcard.go
+++ cart/domain/cart/giftcard.go
@@ -1,6 +1,8 @@
 package cart
 
-import "flamingo.me/flamingo-commerce/v3/price/domain"
+import (
+	"flamingo.me/flamingo-commerce/v3/price/domain"
+)
 
 type (
 	// AppliedGiftCard value object represents a gift card (partial payment) on the cart
@@ -10,13 +12,13 @@ type (
 		Remaining domain.Price // how much of the gift card is still available
 	}
 
-	// AppliedGiftCards
+	// AppliedGiftCards convenience wrapper for array of applied gift cards
 	AppliedGiftCards []AppliedGiftCard
 
 	// WithGiftCard interface for a cart that is able to handle gift cards
 	WithGiftCard interface {
+		HasRemainingGiftCards() bool
 		HasAppliedGiftCards() bool
-		AppliedGiftCards() AppliedGiftCard
 		SumAppliedGiftCards() (domain.Price, error)
 		SumGrandTotalWithGiftCards() (domain.Price, error)
 	}
@@ -27,19 +29,72 @@ var (
 	_ WithGiftCard = &Cart{}
 )
 
+// HasRemaining checks whether gift card has a remaining balance
+func (card AppliedGiftCard) HasRemaining() bool {
+	return !card.Remaining.IsZero()
+}
+
 // HasAppliedGiftCards checks if a gift card is applied to the cart
 func (c Cart) HasAppliedGiftCards() bool {
-	return len(c.AppliedGiftCards()) > 0
+	return len(c.AppliedGiftCards) > 0
 }
 
-func (c Cart) AppliedGiftCards() AppliedGiftCard {
-	panic("implement me")
-}
-
+// SumAppliedGiftCards sum up all applied amounts of giftcads
+// price is returned as a payable
 func (c Cart) SumAppliedGiftCards() (domain.Price, error) {
-	panic("implement me")
+	// guard for no gift cards applied
+	if len(c.AppliedGiftCards) == 0 {
+		return domain.Price{}, nil
+	}
+	prices := make([]domain.Price, 0, len(c.AppliedGiftCards))
+	// add prices to array
+	for _, card := range c.AppliedGiftCards {
+		prices = append(prices, card.Applied)
+	}
+	price, err := domain.SumAll(prices...)
+	// in case of error regarding sum, pass on error
+	if err != nil {
+		return domain.Price{}, nil
+	}
+	return price.GetPayable(), nil
 }
 
+// SumGrandTotalWithGiftCards calculate the grand total of the cart minus gift cards
 func (c Cart) SumGrandTotalWithGiftCards() (domain.Price, error) {
-	panic("implement me")
+	giftCardTotal, err := c.SumAppliedGiftCards()
+	if err != nil {
+		return domain.Price{}, err
+	}
+	// if there are no gift cards just return cart grand total
+	total := c.GrandTotal()
+	if giftCardTotal.IsZero() {
+		return total.GetPayable(), err
+	}
+	// subtract gift card total from total for "remaining total"
+	result, err := total.Sub(giftCardTotal)
+	if err != nil {
+		return domain.Price{}, err
+	}
+	return result.GetPayable(), nil
+}
+
+// HasRemainingGiftCards check whether there are gift cards with remaining balance
+func (c Cart) HasRemainingGiftCards() bool {
+	for _, card := range c.AppliedGiftCards {
+		if card.HasRemaining() {
+			return true
+		}
+	}
+	return false
+}
+
+// ByRemaining fetches gift cards that still have a remaining value from applied gift cards
+func (cards *AppliedGiftCards) ByRemaining() AppliedGiftCards {
+	result := AppliedGiftCards{}
+	for _, card := range *cards {
+		if card.HasRemaining() {
+			result = append(result, card)
+		}
+	}
+	return result
 }
