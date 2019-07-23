68be58b3c114c03c36f0cc287a57d7121e0e0e2e
Leo Back <leo.back@aoe.com>
2019-07-23 15:53:54 +0200

    [Task] #203232 - Split out graphql resolvers into own file

--- cart/interfaces/graphql/service.go
+++ cart/interfaces/graphql/service.go
@@ -1,12 +1,8 @@
 package graphql
 
 import (
-	"context"
-
-	"flamingo.me/flamingo-commerce/v3/cart/application"
 	"flamingo.me/flamingo-commerce/v3/cart/domain/cart"
 	"flamingo.me/flamingo-commerce/v3/cart/domain/decorator"
-	"flamingo.me/flamingo/v3/framework/web"
 	"flamingo.me/graphql"
 	"github.com/99designs/gqlgen/codegen/config"
 )
@@ -35,52 +31,4 @@ func (*Service) Models() map[string]config.TypeMapEntry {
 		"Commerce_CartExistingCustomerData": cart.ExistingCustomerData{},
 		"Commerce_CartPersonalDetails":      cart.PersonalDetails{},
 	}.Models()
-}
-
-// CommerceCartQueryResolver resolver for carts
-type CommerceCartQueryResolver struct {
-	applicationCartReceiverService *application.CartReceiverService
-}
-
-// Inject dependencies
-func (r *CommerceCartQueryResolver) Inject(applicationCartReceiverService *application.CartReceiverService) {
-	r.applicationCartReceiverService = applicationCartReceiverService
-}
-
-// CommerceCart getter for queries
-func (r *CommerceCartQueryResolver) CommerceCart(ctx context.Context) (*decorator.DecoratedCart, error) {
-	req := web.RequestFromContext(ctx)
-
-	return r.applicationCartReceiverService.ViewDecoratedCart(ctx, req.Session())
-}
-
-// CommerceCartMutationResolver resolves cart mutations
-type CommerceCartMutationResolver struct {
-	q                      *CommerceCartQueryResolver
-	applicationCartService *application.CartService
-}
-
-// Inject dependencies
-func (r *CommerceCartMutationResolver) Inject(q *CommerceCartQueryResolver, applicationCartService *application.CartService) {
-	r.q = q
-	r.applicationCartService = applicationCartService
-}
-
-// CommerceAddToCart mutation for adding products to the current users cart
-func (r *CommerceCartMutationResolver) CommerceAddToCart(ctx context.Context, id string, qty *int, deliveryCode string) (*decorator.DecoratedCart, error) {
-	if qty == nil {
-		one := 1
-		qty = &one
-	}
-
-	req := web.RequestFromContext(ctx)
-
-	addRequest := r.applicationCartService.BuildAddRequest(ctx, id, "", *qty)
-
-	_, err := r.applicationCartService.AddProduct(ctx, req.Session(), deliveryCode, addRequest)
-	if err != nil {
-		return nil, err
-	}
-
-	return r.q.CommerceCart(ctx)
 }
