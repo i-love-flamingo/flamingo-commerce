d2c72698dbb19f895e437d415532e12d2d17f148
Leo Back <leo.back@aoe.com>
2019-08-21 11:19:12 +0200

    make qty in AddToCartMutation mandatory

--- cart/interfaces/graphql/resolvers.go
+++ cart/interfaces/graphql/resolvers.go
@@ -41,15 +41,10 @@ func (r *CommerceCartMutationResolver) Inject(q *CommerceCartQueryResolver, appl
 }
 
 // CommerceAddToCart mutation for adding products to the current users cart
-func (r *CommerceCartMutationResolver) CommerceAddToCart(ctx context.Context, marketplaceCode string, qty *int, deliveryCode string) (*decorator.DecoratedCart, error) {
-	if qty == nil {
-		one := 1
-		qty = &one
-	}
-
+func (r *CommerceCartMutationResolver) CommerceAddToCart(ctx context.Context, marketplaceCode string, qty int, deliveryCode string) (*decorator.DecoratedCart, error) {
 	req := web.RequestFromContext(ctx)
 
-	addRequest := r.applicationCartService.BuildAddRequest(ctx, marketplaceCode, "", *qty)
+	addRequest := r.applicationCartService.BuildAddRequest(ctx, marketplaceCode, "", qty)
 
 	_, err := r.applicationCartService.AddProduct(ctx, req.Session(), deliveryCode, addRequest)
 	if err != nil {
