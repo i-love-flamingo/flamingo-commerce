0b4d6098cd015b0de1522cfdc5ab1d31486e1f93
benedikt.blank <benedikt.blank@aoe.com>
2019-08-23 13:26:42 +0200

    Change single CancelOrder function into Guest and Customer function.

--- cart/infrastructure/email/emailplaceorder.go
+++ cart/infrastructure/email/emailplaceorder.go
@@ -69,8 +69,12 @@ func (e *PlaceOrderServiceAdapter) ReserveOrderID(ctx context.Context, cart *car
 	return cart.ID, nil
 }
 
-// CancelOrder cancels a placed order
-func (e *PlaceOrderServiceAdapter) CancelOrder(ctx context.Context, orderInfos placeorder.PlacedOrderInfos) error {
+func (e *PlaceOrderServiceAdapter) CancelGuestOrder(ctx context.Context, orderInfos placeorder.PlacedOrderInfos) error {
+	// since we don't actual place orders we just return nil here
+	return nil
+}
+
+func (e *PlaceOrderServiceAdapter) CancelCustomerOrder(ctx context.Context, orderInfos placeorder.PlacedOrderInfos, auth authDomain.Auth) error {
 	// since we don't actual place orders we just return nil here
 	return nil
 }
