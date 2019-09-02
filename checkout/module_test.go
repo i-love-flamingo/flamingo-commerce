acb7c387cc5ca66662abf8393217b001a27e6aa8
joerg.einfeldt <joerg.einfeldt@aoe.com>
2019-09-02 12:05:05 +0200

    checkout: fix module test

--- checkout/module_test.go
+++ checkout/module_test.go
@@ -1,14 +1,23 @@
 package checkout_test
 
 import (
-	"testing"
+"testing"
 
-	"flamingo.me/flamingo-commerce/v3/checkout"
-	"flamingo.me/dingo"
+"flamingo.me/flamingo-commerce/v3/checkout"
+"flamingo.me/dingo"
+"flamingo.me/flamingo/v3/framework/config"
 )
 
 func TestModule_Configure(t *testing.T) {
-	if err := dingo.TryModule(new(checkout.Module)); err != nil {
+	cfgModule := &config.Module{
+		Map: new(checkout.Module).DefaultConfig(),
+	}
+
+	cfgModule.Map["session.backend"] = ""
+	cfgModule.Map["oauth.useFake"] = true
+	cfgModule.Map["oauth.preventSimultaneousSessions"] = true
+
+	if err := dingo.TryModule(cfgModule, new(checkout.Module)); err != nil {
 		t.Error(err)
 	}
 }
