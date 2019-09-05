ee11ac96869f17daa43c2c11e3c35aa2a235d668
Christopher Schwab <christopher.schwab@aoe.com>
2019-09-05 14:49:17 +0200

    cart. Template function returning qty changes for a given item

---
+++ cart/interfaces/templatefunctions/getItemAdjustment.go
@@ -0,0 +1,58 @@
+package templatefunctions
+
+import (
+	"context"
+
+	"flamingo.me/flamingo-commerce/v3/cart/application"
+	"flamingo.me/flamingo-commerce/v3/cart/domain/cart"
+	"flamingo.me/flamingo/v3/framework/flamingo"
+	"flamingo.me/flamingo/v3/framework/web"
+)
+
+type (
+	// GetItemAdjustment is exported as a template function
+	GetItemAdjustment struct {
+		logger flamingo.Logger
+	}
+
+	QuantityAdjustment struct {
+		item    cart.Item
+		prevQty int
+		currQty int
+	}
+)
+
+// Inject dependencies
+func (tf *GetItemAdjustment) Inject(
+	logger flamingo.Logger,
+
+) {
+	tf.logger = logger.WithField(flamingo.LogKeyModule, "cart").WithField(flamingo.LogKeyCategory, "getItemAdjustment")
+}
+
+// Func defines the GetItemAdjustment template function
+func (tf *GetItemAdjustment) Func(ctx context.Context) interface{} {
+	return func(item cart.Item, deliveryCode string) QuantityAdjustment {
+		session := web.SessionFromContext(ctx)
+
+		if adjustmentsI, found := session.Load("cart.view.adjustment.update"); found {
+			if adjustments, ok := adjustmentsI.([]application.AdjustmentResult); ok {
+				for _, a := range adjustments {
+					if a.Item.ID == item.ID && a.DeliveryCode == deliveryCode {
+						return QuantityAdjustment{
+							item:    item,
+							prevQty: item.Qty - a.RestrictionResult.RemainingDifference,
+							currQty: item.Qty,
+						}
+					}
+				}
+			}
+		}
+
+		return QuantityAdjustment{
+			item:    item,
+			prevQty: item.Qty,
+			currQty: item.Qty,
+		}
+	}
+}
