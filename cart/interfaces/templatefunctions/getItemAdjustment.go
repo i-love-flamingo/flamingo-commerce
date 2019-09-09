eb42895b70fd93ae33991b2427b0ecd636515818
Christopher Schwab <christopher.schwab@aoe.com>
2019-09-09 15:13:26 +0200

    cart: Export QuantityAdjustment fields to make them visible in frontend

--- cart/interfaces/templatefunctions/getItemAdjustment.go
+++ cart/interfaces/templatefunctions/getItemAdjustment.go
@@ -4,56 +4,46 @@ import (
 	"context"
 
 	"flamingo.me/flamingo-commerce/v3/cart/application"
-	"flamingo.me/flamingo-commerce/v3/cart/domain/cart"
-	"flamingo.me/flamingo/v3/framework/flamingo"
 	"flamingo.me/flamingo/v3/framework/web"
+
+	"flamingo.me/flamingo-commerce/v3/cart/domain/cart"
 )
 
 type (
 	// GetItemAdjustment is exported as a template function
-	GetItemAdjustment struct {
-		logger flamingo.Logger
-	}
+	GetItemAdjustment struct{}
 
 	// QuantityAdjustment is returned by the template function
 	QuantityAdjustment struct {
-		item    cart.Item
-		prevQty int
-		currQty int
+		Item    cart.Item
+		PrevQty int
+		CurrQty int
 	}
 )
 
-// Inject dependencies
-func (tf *GetItemAdjustment) Inject(
-	logger flamingo.Logger,
-
-) {
-	tf.logger = logger.WithField(flamingo.LogKeyModule, "cart").WithField(flamingo.LogKeyCategory, "getItemAdjustment")
-}
-
 // Func defines the GetItemAdjustment template function
-func (tf *GetItemAdjustment) Func(ctx context.Context) interface{} {
-	return func(item cart.Item, deliveryCode string) QuantityAdjustment {
+func (gia *GetItemAdjustment) Func(ctx context.Context) interface{} {
+	return func(item cart.Item, deliveryCode string) interface{} {
 		session := web.SessionFromContext(ctx)
 
 		if adjustmentsI, found := session.Load("cart.view.adjustment.update"); found {
 			if adjustments, ok := adjustmentsI.(application.QtyAdjustmentResults); ok {
 				for _, a := range adjustments {
 					if a.Item.ID == item.ID && a.DeliveryCode == deliveryCode {
-						return QuantityAdjustment{
-							item:    item,
-							prevQty: item.Qty - a.RestrictionResult.RemainingDifference,
-							currQty: item.Qty,
+						return &QuantityAdjustment{
+							Item:    item,
+							PrevQty: item.Qty - a.RestrictionResult.RemainingDifference,
+							CurrQty: item.Qty,
 						}
 					}
 				}
 			}
 		}
 
-		return QuantityAdjustment{
-			item:    item,
-			prevQty: item.Qty,
-			currQty: item.Qty,
+		return &QuantityAdjustment{
+			Item:    item,
+			PrevQty: item.Qty,
+			CurrQty: item.Qty,
 		}
 	}
 }
