f8c2577d847402bb9b00871e815628788e23eb61
Dominik Dumont <dominik.dumont@aoe.com>
2019-10-16 16:56:02 +0200

    Remove additional billing data query and update error keys

--- cart/interfaces/graphql/resolvers.go
+++ cart/interfaces/graphql/resolvers.go
@@ -2,11 +2,9 @@ package graphql
 
 import (
 	"context"
-	"flamingo.me/flamingo-commerce/v3/cart/interfaces/controller/forms"
-	"flamingo.me/flamingo-commerce/v3/cart/interfaces/graphql/dto"
-
 	"flamingo.me/flamingo-commerce/v3/cart/application"
 	"flamingo.me/flamingo-commerce/v3/cart/domain/decorator"
+	"flamingo.me/flamingo-commerce/v3/cart/interfaces/controller/forms"
 	"flamingo.me/flamingo/v3/framework/web"
 )
 
@@ -20,23 +18,10 @@ type CommerceCartQueryResolver struct {
 func (r *CommerceCartQueryResolver) Inject(applicationCartReceiverService *application.CartReceiverService, billingAddressFormController *forms.BillingAddressFormController) {
 	r.applicationCartReceiverService = applicationCartReceiverService
 	r.billingAddressFormController = billingAddressFormController
-
 }
 
 // CommerceCart getter for queries
 func (r *CommerceCartQueryResolver) CommerceCart(ctx context.Context) (*decorator.DecoratedCart, error) {
 	req := web.RequestFromContext(ctx)
 	return r.applicationCartReceiverService.ViewDecoratedCart(ctx, req.Session())
-}
-
-//CommerceCartGetBillingAddressForm getter
-func (r *CommerceCartQueryResolver) CommerceCartGetBillingAddressForm(ctx context.Context) (*dto.BillingAddressForm, error) {
-	req := web.RequestFromContext(ctx)
-	billingForm, err := r.billingAddressFormController.GetUnsubmittedForm(ctx, req)
-	if err != nil {
-		return nil, err
-	}
-
-	return mapCommerceBillingAddressForm(billingForm, false)
-
 }
