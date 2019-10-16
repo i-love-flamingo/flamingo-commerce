43dbeb1e0f491802d04d292f9e5069161941741b
Dominik Dumont <dominik.dumont@aoe.com>
2019-10-16 17:52:44 +0200

    Remove unused di

--- cart/interfaces/graphql/resolvers.go
+++ cart/interfaces/graphql/resolvers.go
@@ -4,20 +4,19 @@ import (
 	"context"
 	"flamingo.me/flamingo-commerce/v3/cart/application"
 	"flamingo.me/flamingo-commerce/v3/cart/domain/decorator"
-	"flamingo.me/flamingo-commerce/v3/cart/interfaces/controller/forms"
 	"flamingo.me/flamingo/v3/framework/web"
 )
 
 // CommerceCartQueryResolver resolver for carts
 type CommerceCartQueryResolver struct {
 	applicationCartReceiverService *application.CartReceiverService
-	billingAddressFormController   *forms.BillingAddressFormController
 }
 
 // Inject dependencies
-func (r *CommerceCartQueryResolver) Inject(applicationCartReceiverService *application.CartReceiverService, billingAddressFormController *forms.BillingAddressFormController) {
+func (r *CommerceCartQueryResolver) Inject(
+	applicationCartReceiverService *application.CartReceiverService,
+) {
 	r.applicationCartReceiverService = applicationCartReceiverService
-	r.billingAddressFormController = billingAddressFormController
 }
 
 // CommerceCart getter for queries
