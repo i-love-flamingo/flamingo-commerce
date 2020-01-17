b2740eb886e2f3b9decbf16906259a40124a00db
Carsten Dietrich <carsten.dietrich@aoe.com>
2020-01-17 16:25:11 +0100

    checkout: Add dummy implementation for CompletePayment/ValidatePayment state

---
+++ checkout/domain/placeorder/states/complete_payment.go
@@ -0,0 +1,68 @@
+package states
+
+import (
+	"context"
+	"encoding/gob"
+
+	"flamingo.me/flamingo-commerce/v3/checkout/domain/placeorder/process"
+	"flamingo.me/flamingo-commerce/v3/payment/interfaces"
+)
+
+type (
+	// CompletePayment state
+	CompletePayment struct {
+		paymentGateway interfaces.WebCartPaymentGateway
+	}
+)
+
+var _ process.State = CompletePayment{}
+
+func init() {
+	gob.Register(CompletePayment{})
+}
+
+// Inject dependencies
+func (c *CompletePayment) Inject(
+	paymentGateway interfaces.WebCartPaymentGateway,
+) *CompletePayment {
+	c.paymentGateway = paymentGateway
+
+	return c
+}
+
+// Name get state name
+func (CompletePayment) Name() string {
+	return "CompletePayment"
+}
+
+// Run the state operations
+func (c CompletePayment) Run(ctx context.Context, p *process.Process) process.RunResult {
+	cart := p.Context().Cart
+
+	payment, err := c.paymentGateway.OrderPaymentFromFlow(ctx, &cart, p.Context().UUID)
+	if err != nil {
+		return process.RunResult{
+			Failed: process.ErrorOccurredReason{Error: err.Error()},
+		}
+	}
+
+	err = c.paymentGateway.ConfirmResult(ctx, &cart, payment)
+	if err != nil {
+		return process.RunResult{
+			Failed: process.ErrorOccurredReason{Error: err.Error()},
+		}
+	}
+
+	p.UpdateState(ValidatePayment{})
+	return process.RunResult{}
+}
+
+// Rollback the state operations
+func (c CompletePayment) Rollback(_ process.RollbackData) error {
+	return nil
+}
+
+// IsFinal if state is a final state
+func (c CompletePayment) IsFinal() bool {
+	return false
+}
--- checkout/domain/placeorder/states/validate_payment.go
+++ checkout/domain/placeorder/states/validate_payment.go
@@ -5,13 +5,15 @@ import (
 	"encoding/gob"
 
 	"flamingo.me/flamingo-commerce/v3/checkout/domain/placeorder/process"
+	paymentDomain "flamingo.me/flamingo-commerce/v3/payment/domain"
 	"flamingo.me/flamingo-commerce/v3/payment/interfaces"
 )
 
 type (
 	// ValidatePayment state
 	ValidatePayment struct {
-		paymentGateway interfaces.WebCartPaymentGateway
+		paymentGateway  interfaces.WebCartPaymentGateway
+		EarlyPlaceOrder bool
 	}
 )
 
@@ -21,6 +23,15 @@ func init() {
 	gob.Register(ValidatePayment{})
 }
 
+// Inject dependencies
+func (v *ValidatePayment) Inject(
+	paymentGateway interfaces.WebCartPaymentGateway,
+) *ValidatePayment {
+	v.paymentGateway = paymentGateway
+
+	return v
+}
+
 // Name get state name
 func (ValidatePayment) Name() string {
 	return "ValidatePayment"
@@ -28,6 +39,40 @@ func (ValidatePayment) Name() string {
 
 // Run the state operations
 func (v ValidatePayment) Run(ctx context.Context, p *process.Process) process.RunResult {
+	cart := p.Context().Cart
+	flowStatus, err := v.paymentGateway.FlowStatus(ctx, &cart, p.Context().UUID)
+	if err != nil {
+		return process.RunResult{
+			Failed: process.ErrorOccurredReason{Error: err.Error()},
+		}
+	}
+
+	switch flowStatus.Status {
+	case paymentDomain.PaymentFlowStatusUnapproved:
+		// payment just started, frontend needs to do actions
+		p.UpdateState(Wait{})
+	case paymentDomain.PaymentFlowStatusApproved:
+		// payment is done but needs confirmation
+		p.UpdateState(CompletePayment{})
+	case paymentDomain.PaymentFlowStatusCompleted:
+		// payment is done and confirmed, place order if not already placed
+		if v.EarlyPlaceOrder {
+			p.UpdateState(Success{})
+		} else {
+			p.UpdateState(PlaceOrder{})
+		}
+	case paymentDomain.PaymentFlowStatusAborted, paymentDomain.PaymentFlowStatusFailed, paymentDomain.PaymentFlowStatusCancelled:
+		return process.RunResult{
+			Failed: process.ErrorOccurredReason{Error: flowStatus.Status},
+		}
+	case paymentDomain.PaymentFlowWaitingForCustomer:
+		// payment pending, waiting for customer doing async stuff
+		p.UpdateState(Wait{})
+	default:
+		// unknown payment flowStatus, let frontend handle it
+		p.UpdateState(Wait{})
+	}
+
 	return process.RunResult{}
 }
 
