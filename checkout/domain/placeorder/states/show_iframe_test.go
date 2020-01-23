9a914d46389bceef1c2258f6976bd9d5a3b01950
Carsten Dietrich <carsten.dietrich@aoe.com>
2020-01-23 12:21:16 +0100

    checkout: Add payment states

---
+++ checkout/domain/placeorder/states/show_iframe_test.go
@@ -0,0 +1,36 @@
+package states_test
+
+import (
+	"context"
+	"testing"
+
+	"flamingo.me/flamingo-commerce/v3/checkout/domain/placeorder/process"
+	"flamingo.me/flamingo-commerce/v3/checkout/domain/placeorder/states"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestShowIframe_IsFinal(t *testing.T) {
+	s := states.ShowIframe{}
+	assert.False(t, s.IsFinal())
+}
+
+func TestShowIframe_Name(t *testing.T) {
+	s := states.ShowIframe{}
+	assert.Equal(t, "ShowIframe", s.Name())
+}
+
+func TestShowIframe_Rollback(t *testing.T) {
+	s := states.ShowIframe{}
+	assert.Nil(t, s.Rollback(nil))
+}
+
+func TestShowIframe_Run(t *testing.T) {
+	s := states.ShowIframe{}
+	p := &process.Process{}
+
+	s.Run(context.Background(), p)
+
+	assert.Equal(t, states.ValidatePayment{}.Name(), p.Context().State, "Next state should be ValidatePayment.")
+
+	assert.Equal(t, s.Run(context.Background(), &process.Process{}), process.RunResult{})
+}
