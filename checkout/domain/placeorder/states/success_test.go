2e880c8cafea82d7403ef83312936becce40d6f1
Carsten Dietrich <carsten.dietrich@aoe.com>
2020-01-27 16:59:02 +0100

    checkout: move state test in separate package

--- checkout/domain/placeorder/states/success_test.go
+++ checkout/domain/placeorder/states/success_test.go
@@ -1,29 +1,30 @@
-package states
+package states_test
 
 import (
 	"context"
 	"testing"
 
 	"flamingo.me/flamingo-commerce/v3/checkout/domain/placeorder/process"
+	"flamingo.me/flamingo-commerce/v3/checkout/domain/placeorder/states"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestSuccess_IsFinal(t *testing.T) {
-	s := Success{}
+	s := states.Success{}
 	assert.True(t, s.IsFinal())
 }
 
 func TestSuccess_Name(t *testing.T) {
-	s := Success{}
+	s := states.Success{}
 	assert.Equal(t, "Success", s.Name())
 }
 
 func TestSuccess_Rollback(t *testing.T) {
-	s := Success{}
+	s := states.Success{}
 	assert.Nil(t, s.Rollback(context.Background(), nil))
 }
 
 func TestSuccess_Run(t *testing.T) {
-	s := Success{}
+	s := states.Success{}
 	assert.Equal(t, s.Run(context.Background(), &process.Process{}), process.RunResult{})
 }
