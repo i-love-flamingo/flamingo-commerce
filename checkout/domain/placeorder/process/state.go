4ae565d70df4a2f94251e9584939b99c4915471e
Carsten Dietrich <carsten.dietrich@aoe.com>
2020-02-03 10:26:40 +0100

    checkout: add factory method for fatal rollback errors

--- checkout/domain/placeorder/process/state.go
+++ checkout/domain/placeorder/process/state.go
@@ -24,11 +24,20 @@ type (
 	// FatalRollbackError which causes the premature end of rollback process
 	FatalRollbackError struct {
 		error error
+		State string
 	}
 
 	// PaymentValidatorFunc to decide over next state depending on payment situation
 	PaymentValidatorFunc func(ctx context.Context, p *Process, paymentService *application.PaymentService) RunResult
 )
+
+// NewFatalRollbackError mark a rollback error as fatal, this breaks the current rollback
+func NewFatalRollbackError(err error, stateName string) FatalRollbackError {
+	return FatalRollbackError{
+		error: err,
+		State: stateName,
+	}
+}
 
 func (f *FatalRollbackError) Error() string {
 	return f.error.Error()
