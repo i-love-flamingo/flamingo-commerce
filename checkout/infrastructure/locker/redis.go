1c1d1e2b7765d8ae83dc135d402a5b973bc9e200
Thorsten Essig <thorsten.essig@aoe.com>
2020-02-06 17:44:56 +0100

    checkout: implement redis lock

---
+++ checkout/infrastructure/locker/redis.go
@@ -0,0 +1,89 @@
+package locker
+
+import (
+	"errors"
+	"time"
+
+	"github.com/go-redsync/redsync"
+	"github.com/gomodule/redigo/redis"
+
+	"flamingo.me/flamingo-commerce/v3/checkout/application/placeorder"
+)
+
+type (
+	// Redis TryLock for nin clustered applications
+	Redis struct {
+		redsync     *redsync.Redsync
+		network     string
+		address     string
+		maxIdle     int
+		idleTimeout time.Duration
+	}
+)
+
+// NewRedis creates a new distributed mutex using multiple Redis connection pools.
+func NewRedis(
+	cfg *struct {
+		MaxIdle                 int    `inject:"config:commerce.checkout.placeorder.lock.redis.maxIdle"`
+		IdleTimeoutMilliseconds int    `inject:"config:config:commerce.checkout.placeorder.lock.redis.idleTimeoutMilliseconds"`
+		Network                 string `inject:"config:config:commerce.checkout.placeorder.lock.redis.network"`
+		Address                 string `inject:"config:config:commerce.checkout.placeorder.lock.redis.address"`
+	},
+) *Redis {
+	r := new(Redis)
+
+	if cfg != nil {
+		r.maxIdle = cfg.MaxIdle
+		r.idleTimeout = time.Duration(cfg.IdleTimeoutMilliseconds) * time.Millisecond
+		r.network = cfg.Network
+		r.address = cfg.Address
+	}
+
+	pools := []redsync.Pool{&redis.Pool{
+		MaxIdle:     r.maxIdle,
+		IdleTimeout: r.idleTimeout,
+		Dial: func() (redis.Conn, error) {
+			return redis.Dial(r.network, r.address)
+		},
+		TestOnBorrow: func(c redis.Conn, t time.Time) error {
+			_, err := c.Do("PING")
+			return err
+		},
+	}}
+
+	r.redsync = redsync.New(pools)
+
+	return r
+}
+
+var _ placeorder.TryLock = &Redis{}
+
+// TryLock ties once to acquire a lock and returns the unlock func if successful
+func (r *Redis) TryLock(key string, maxlockduration time.Duration) (placeorder.Unlock, error) {
+	mutex := r.redsync.NewMutex(
+		key,
+		redsync.SetExpiry(maxlockduration),
+		redsync.SetTries(1),
+		redsync.SetRetryDelayFunc(func(int) time.Duration { return 50 * time.Millisecond }),
+	)
+	err := mutex.Lock()
+	if err != nil {
+		return nil, err
+	}
+	ticker := time.NewTicker(maxlockduration / 3)
+	go func() {
+		for {
+			<-ticker.C
+			mutex.Extend()
+		}
+	}()
+
+	return func() error {
+		ok := mutex.Unlock()
+		if !ok {
+			return errors.New("unlock unsuccessful")
+		}
+		ticker.Stop()
+		return nil
+	}, nil
+}
---
+++ checkout/infrastructure/locker/redis_test.go
@@ -0,0 +1,107 @@
+package locker_test
+
+import (
+	"fmt"
+	"os/exec"
+	"testing"
+	"time"
+
+	"github.com/gomodule/redigo/redis"
+	"github.com/ory/dockertest"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/stvp/tempredis"
+
+	"flamingo.me/flamingo-commerce/v3/checkout/infrastructure/locker"
+)
+
+func startUp(t *testing.T) *tempredis.Server {
+	t.Helper()
+	server, err := tempredis.Start(tempredis.Config{})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	return server
+}
+
+func getRedisLocker(network, address string) *locker.Redis {
+	redis := locker.NewRedis(&struct {
+		MaxIdle                 int    `inject:"config:commerce.checkout.placeorder.lock.redis.maxIdle"`
+		IdleTimeoutMilliseconds int    `inject:"config:config:commerce.checkout.placeorder.lock.redis.idleTimeoutMilliseconds"`
+		Network                 string `inject:"config:config:commerce.checkout.placeorder.lock.redis.network"`
+		Address                 string `inject:"config:config:commerce.checkout.placeorder.lock.redis.address"`
+	}{MaxIdle: 3, IdleTimeoutMilliseconds: 240000, Network: network, Address: address})
+	return redis
+}
+
+func TestRedis_TryLockDocker(t *testing.T) {
+	pool, err := dockertest.NewPool("")
+	if err != nil {
+		t.Skip("docker not installed")
+	}
+
+	res, err := pool.Run("redis", "5.0", nil)
+	require.NoError(t, err, "failed to run redis docker image")
+	defer pool.Purge(res)
+	address := fmt.Sprintf("%s:%s", "127.0.0.1", res.GetPort("6379/tcp"))
+
+	require.NoError(t, pool.Retry(func() error {
+		_, err := redis.Dial("tcp", address)
+		return err
+	}),
+	)
+
+	redisLocker := getRedisLocker("tcp", address)
+	runTestCases(t, redisLocker)
+}
+
+func TestRedis_TryLock(t *testing.T) {
+	if _, err := exec.LookPath("redis-server"); err != nil {
+		t.Skip("redis-server not installed")
+	}
+
+	server := startUp(t)
+	defer server.Term()
+	redisLocker := getRedisLocker("unix", server.Socket())
+
+	runTestCases(t, redisLocker)
+
+}
+
+func runTestCases(t *testing.T, redisLocker *locker.Redis) {
+	t.Run("really locked", func(t *testing.T) {
+		key := "test"
+		start := time.Now()
+		// get a long lock
+		unlock, err := redisLocker.TryLock(key, time.Minute)
+		require.NoError(t, err)
+
+		// try to get same lock
+		_, err = redisLocker.TryLock(key, time.Second)
+		assert.Error(t, err)
+		// assert if we were really in the lock period
+		assert.True(t, time.Now().Before(start.Add(time.Minute)))
+
+		// unlock
+		require.NoError(t, unlock())
+
+		// get the lock successfully again after unlock
+		unlock, err = redisLocker.TryLock(key, time.Minute)
+		require.NoError(t, err)
+		require.NoError(t, unlock())
+	})
+
+	t.Run("lock should be expanded", func(t *testing.T) {
+		key := "test_expanded"
+
+		unlock, err := redisLocker.TryLock(key, 100*time.Millisecond)
+		require.NoError(t, err)
+		defer unlock()
+
+		time.Sleep(200 * time.Millisecond)
+		// try to get same lock
+		_, err = redisLocker.TryLock(key, time.Second)
+		assert.Error(t, err)
+	})
+}
