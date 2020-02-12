0eea1e109af9b74c7f4cb6ebc557a4790ad06c97
Carsten Dietrich <carsten.dietrich@aoe.com>
2020-02-12 15:41:52 +0100

    checkout: Update readme with details about locking, rename simple lock to memory lock

--- checkout/infrastructure/locker/memory.go
+++ checkout/infrastructure/locker/memory.go
@@ -10,18 +10,18 @@ import (
 )
 
 type (
-	//Simple TryLock for nin clustered applications
-	Simple struct {
+	// Memory TryLock for non clustered applications
+	Memory struct {
 		m sync.Mutex
 	}
 )
 
-var _ placeorder.TryLock = &Simple{}
+var _ placeorder.TryLock = &Memory{}
 
 const mutexLocked = 1 << iota
 
 // TryLock unblocking implementation - see https://github.com/LK4D4/trylock/blob/master/trylock.go
-func (s *Simple) TryLock(key string, maxlockduration time.Duration) (placeorder.Unlock, error) {
+func (s *Memory) TryLock(key string, maxlockduration time.Duration) (placeorder.Unlock, error) {
 	haveLock := atomic.CompareAndSwapInt32((*int32)(unsafe.Pointer(&s.m)), 0, mutexLocked)
 	if !haveLock {
 		return nil, placeorder.ErrLockTaken
