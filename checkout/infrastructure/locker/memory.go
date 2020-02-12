79cf39559f3cb0cf6113c9dcf3f989bb63dd4e9d
Carsten Dietrich <carsten.dietrich@aoe.com>
2020-02-12 16:01:31 +0100

    checkout: Add todo to in memory lock

--- checkout/infrastructure/locker/memory.go
+++ checkout/infrastructure/locker/memory.go
@@ -20,8 +20,9 @@ var _ placeorder.TryLocker = &Memory{}
 
 const mutexLocked = 1 << iota
 
-// TryLocker unblocking implementation - see https://github.com/LK4D4/trylock/blob/master/trylock.go
+// TryLock unblocking implementation see https://github.com/LK4D4/trylock/blob/master/trylock.go
 func (s *Memory) TryLock(key string, _ time.Duration) (placeorder.Unlock, error) {
+	// Todo: support multiple locks based on lock key
 	haveLock := atomic.CompareAndSwapInt32((*int32)(unsafe.Pointer(&s.m)), 0, mutexLocked)
 	if !haveLock {
 		return nil, placeorder.ErrLockTaken
