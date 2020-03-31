645a8e0cfbcf98dfa2f1711c95f1d72622ddb63e
Thorsten Essig <thorsten.essig@aoe.com>
2020-03-31 17:43:30 +0200

    integrationtest: regenerate graphql

--- test/integrationtest/projecttest/graphql/generated.go
+++ test/integrationtest/projecttest/graphql/generated.go
@@ -23659,7 +23659,7 @@ func (ec *executionContext) unmarshalOString2ᚕstring(ctx context.Context, v in
 	var err error
 	res := make([]string, len(vSlice))
 	for i := range vSlice {
-		res[i], err = ec.unmarshalOString2string(ctx, vSlice[i])
+		res[i], err = ec.unmarshalNString2string(ctx, vSlice[i])
 		if err != nil {
 			return nil, err
 		}
@@ -23673,7 +23673,7 @@ func (ec *executionContext) marshalOString2ᚕstring(ctx context.Context, sel as
 	}
 	ret := make(graphql.Array, len(v))
 	for i := range v {
-		ret[i] = ec.marshalOString2string(ctx, sel, v[i])
+		ret[i] = ec.marshalNString2string(ctx, sel, v[i])
 	}
 
 	return ret
