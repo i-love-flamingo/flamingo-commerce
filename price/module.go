097d481ea31f5257fbbc38bbed9de5dcad8fd368
Daniel Pötzinger <daniel.poetzinger@aoe.com>
2020-05-15 13:41:25 +0200

    price: add locale as dependend module

--- price/module.go
+++ price/module.go
@@ -4,6 +4,7 @@ import (
 	"flamingo.me/dingo"
 	pricegraphql "flamingo.me/flamingo-commerce/v3/price/interfaces/graphql"
 	"flamingo.me/flamingo-commerce/v3/price/interfaces/templatefunctions"
+	"flamingo.me/flamingo/v3/core/locale"
 	"flamingo.me/flamingo/v3/framework/flamingo"
 	"flamingo.me/graphql"
 )
@@ -17,4 +18,11 @@ type (
 func (m Module) Configure(injector *dingo.Injector) {
 	flamingo.BindTemplateFunc(injector, "commercePriceFormat", new(templatefunctions.CommercePriceFormatFunc))
 	injector.BindMulti(new(graphql.Service)).To(pricegraphql.Service{})
+}
+
+// Depends adds our dependencies
+func (*Module) Depends() []dingo.Module {
+	return []dingo.Module{
+		new(locale.Module),
+	}
 }
