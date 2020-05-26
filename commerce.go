e178d3a498756af4820a19204e8960bf7689ee3b
Daniel Pötzinger <daniel.poetzinger@aoe.com>
2020-05-26 20:35:29 +0200

    adjust package name

--- commerce.go
+++ commerce.go
@@ -1,8 +1,13 @@
-package main_test
+package commerce
+
+/*
+	Flamingo Commerce Modules.
+	The subpackage represent Flamingo Commerce modules - please refer to the documentations for the individual modules.
+ */
 
 //go:generate go run github.com/swaggo/swag/cmd/swag init --generalInfo=openapidoc.go --dir=./ --output=docs/openapi
 
-// ***** Swagger Documentation annotation for generator swag (https://github.com/swaggo/swag#declarative-comments-format) ******
+// Swagger Documentation used for generator swag (https://github.com/swaggo/swag#declarative-comments-format)
 // @title Flamingo Commerce API Spec
 // @description Swagger (OpenAPI) Spec of all Flamingo Commerce modules
 // @version 1.0
@@ -10,6 +15,5 @@ package main_test
 // @contact.url https://gitter.im/i-love-flamingo/community#
 // @contact.email flamingo@aoe.com
 // @license.name MIT
-// *****
 // @tag.name v1 Cart ajax API
 // @tag.description This Cart APIs are most suitable to be called from a browser, because they rely on the session and cookie headers.
