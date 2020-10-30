2d7ef218132a538c5e6b65055de0b3fa676b1c92
nicole.schmelzer <nicole.schmelzer@aoe.com>
2020-10-30 10:19:13 +0100

    Updated readme for product module regarding fake search service

--- product/infrastructure/fake/searchservice.go
+++ product/infrastructure/fake/searchservice.go
@@ -61,7 +61,7 @@ func (s *SearchService) SearchBy(ctx context.Context, _ string, _ []string, _ ..
 func (s *SearchService) findProducts(ctx context.Context, filters []searchDomain.Filter) []domain.BasicProduct {
 	products := make([]domain.BasicProduct, 0)
 
-	// - try finding product by marketPlaceCode given in query or return nothing if query is not-found
+	// - try finding product by marketPlaceCode given in query or return nothing if query is no-results
 	if query, found := s.filterValue(filters, "q"); found {
 		if len(query) > 0 {
 			if query[0] == "no-results" {
