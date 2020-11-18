94c263dc27111fc03ad30b73049fa8535982b221
Thomas Neumann <thomas.neumann@aoe.com>
2020-11-18 12:02:30 +0100

    Trying to fix race on inmemory cart storage
    The Locker is not initialized together with the inmemory cart storage on Inject - which must be called to use the inmemory storage.

--- cart/infrastructure/inMemoryStorage.go
+++ cart/infrastructure/inMemoryStorage.go
@@ -18,18 +18,20 @@ type (
 
 var _ CartStorage = &InMemoryCartStorage{}
 
-func (s *InMemoryCartStorage) init() {
-	if s.guestCarts == nil {
-		s.guestCarts = make(map[string]*domaincart.Cart)
-		s.locker = &sync.Mutex{}
-	}
+// Inject dependencies and prepare storage
+// Important: InMemoryStorage MUST be bound AsEagerSingleton, Inject MUST be called in tests to behave as expected
+func (s *InMemoryCartStorage) Inject() *InMemoryCartStorage {
+	s.locker = &sync.Mutex{}
+	s.guestCarts = make(map[string]*domaincart.Cart)
+
+	return s
 }
 
 // HasCart checks if the cart storage has a cart with a given id
 func (s *InMemoryCartStorage) HasCart(_ context.Context, id string) bool {
-	s.init()
 	s.locker.Lock()
 	defer s.locker.Unlock()
+
 	if _, ok := s.guestCarts[id]; ok {
 		return true
 	}
@@ -38,9 +40,9 @@ func (s *InMemoryCartStorage) HasCart(_ context.Context, id string) bool {
 
 // GetCart returns a cart with the given id from the cart storage
 func (s *InMemoryCartStorage) GetCart(_ context.Context, id string) (*domaincart.Cart, error) {
-	s.init()
 	s.locker.Lock()
 	defer s.locker.Unlock()
+
 	if cart, ok := s.guestCarts[id]; ok {
 		return cart, nil
 	}
@@ -49,18 +51,18 @@ func (s *InMemoryCartStorage) GetCart(_ context.Context, id string) (*domaincart
 
 // StoreCart stores a cart in the storage
 func (s *InMemoryCartStorage) StoreCart(_ context.Context, cart *domaincart.Cart) error {
-	s.init()
 	s.locker.Lock()
 	defer s.locker.Unlock()
+
 	s.guestCarts[cart.ID] = cart
 	return nil
 }
 
 // RemoveCart from storage
 func (s *InMemoryCartStorage) RemoveCart(_ context.Context, cart *domaincart.Cart) error {
-	s.init()
 	s.locker.Lock()
 	defer s.locker.Unlock()
+
 	delete(s.guestCarts, cart.ID)
 	return nil
 }
