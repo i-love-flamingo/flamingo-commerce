a4a8190ef390ade3529f47c92e512954f0ef4f35
Carsten Dietrich <3203968+carstendietrich@users.noreply.github.com>
2021-07-12 11:17:15 +0200

    fix cue default config indent

--- checkout/module.go
+++ checkout/module.go
@@ -112,7 +112,7 @@ commerce: checkout: {
 		network:                 string | *"tcp"
 		address:                 string | *"localhost:6379"
 		database:                number | *0
-		ttl:					 string | *"2h"
+		ttl:                     string | *"2h"
 	}
 	activateDeprecatedSourcing:		    bool | *false
 	useDeliveryForms:                 bool | *true
