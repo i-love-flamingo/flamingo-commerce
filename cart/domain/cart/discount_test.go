4b7f1e1c8929bb4ade5190d7341b639d402a8857
dylan.provost <dylan.provost@omnevo.net>
2025-05-26 09:13:27 +0200

    feat(cart): add custom attributes to applied discounts

--- cart/domain/cart/discount_test.go
+++ cart/domain/cart/discount_test.go
@@ -499,6 +499,17 @@ func TestDelivery_MergeDiscounts(t *testing.T) {
 					Applied:      domain.NewFromFloat(-10.0, "$"),
 					SortOrder:    5,
 				},
+				{
+					CampaignCode: "code-7",
+					Label:        "title-7",
+					Type:         "type-7",
+					Applied:      domain.NewFromFloat(-10.0, "$"),
+					SortOrder:    6,
+					CustomAttributes: map[string]interface{}{
+						"attr1": 3,
+						"attr2": 1,
+					},
+				},
 			},
 		},
 		{
